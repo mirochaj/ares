e72ccf8d8fbd08e9635826c3705a7f33aa266cc9
Jacob Jost <jajo2581@colorado.edu>
2015-06-15 16:22:15 -0600

    Should have fixed merging changes'

--- input/litdata/robertson2015.py
+++ input/litdata/robertson2015.py
@@ -27,6 +27,12 @@ def _SFRD(z, a=None, b=None, c=None, d=None):
     
     return a * (1. + z)**b / (1 + ((1 + z) / c)**d)
     
+    
 def SFRD(z):
     
     return _SFRD(z, **sfrd_pars) 
+def SFRD(z, **kwargs):
+    if not kwargs:
+        kwargs = sfrd_pars
+       
+    return _SFRD(z, **kwargs)
