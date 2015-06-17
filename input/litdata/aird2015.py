4a2667844443521bb1d20f150804a5e7f939a412
Jacob Jost <jajo2581@colorado.edu>
2015-06-17 11:50:02 -0600

    consolidated and added randomsample function in arid2015 and ueda2014, removed unneeded file

--- input/litdata/aird2015.py
+++ input/litdata/aird2015.py
@@ -25,6 +25,8 @@ NOTE:
     --- = spacing between different sections of code
     
     ### = spacing within a section of code to denote a different section within that particular code
+    
+    $$$ = spacing between different parts of codes
 
 """
 
@@ -50,20 +52,20 @@ qsolf_LDDE2_softpars = \
 
 qsolf_LDDE2_softerr = \
 {
- 'K': 10**0.05,#???
- 'loglstar': 10**0.03,
- 'gamma1': 0.01,
- 'gamma2': 0.05,
- 'p1': 0.35,
- 'p2': -0.12,
- 'p3': -0.51,
- 'beta1': 0.06,
- 'zstar': 0.08,
- 'zstarc2': 0.11,
- 'logLa': 10**0.12,
- 'logLa2': 10**0.30,
- 'alpha': 0.01,
- 'alpha2': 0.02
+ 'K_err': 10**0.05,#???
+ 'loglstar_err': 10**0.03,
+ 'gamma1_err': 0.01,
+ 'gamma2_err': 0.05,
+ 'p1_err': 0.35,
+ 'p2_err': 0.12,
+ 'p3_err': 0.51,
+ 'beta1_err': 0.06,
+ 'zstar_err': 0.08,
+ 'zstarc2_err': 0.11,
+ 'logLa_err': 10**0.12,
+ 'logLa2_err': 10**0.30,
+ 'alpha_err': 0.01,
+ 'alpha2_err': 0.02
 }
 
 qsolf_LDDE1_softpars = \
@@ -81,15 +83,15 @@ qsolf_LDDE1_softpars = \
 
 qsolf_LDDE1_softerr = \
 {
- 'K': 10**0.05,#???
- 'loglstar': 10**0.04,
- 'gamma1': 0.02,
- 'gamma2': 0.06,
- 'p1': 0.09,
- 'p2': 0.14,
- 'zstar': 0.09,
- 'logLa': 10**0.11,
- 'alpha': 0.01,
+ 'K_err': 10**0.05,#???
+ 'loglstar_err': 10**0.04,
+ 'gamma1_err': 0.02,
+ 'gamma2_err': 0.06,
+ 'p1_err': 0.09,
+ 'p2_err': 0.14,
+ 'zstar_err': 0.09,
+ 'logLa_err': 10**0.11,
+ 'alpha_err': 0.01,
 }
 ##################################################
 qsolf_LDDE2_hardpars = \
@@ -112,20 +114,20 @@ qsolf_LDDE2_hardpars = \
 
 qsolf_LDDE2_harderr = \
 {
- 'K': 10**0.07,#???
- 'loglstar': 10**0.05,
- 'gamma1': 0.02,
- 'gamma2': 0.06,
- 'p1': 0.18,
- 'p2': 0.13,
- 'p3': 0.62,
- 'beta1': 0.09,
- 'zstar': 0.08,
- 'zstarc2': 0.10,
- 'logLa': 10**0.07,
- 'logLa2': 10**0.17,
- 'alpha': 0.01,
- 'alpha2': 0.02
+ 'K_err': 10**0.07,#???
+ 'loglstar_err': 10**0.05,
+ 'gamma1_err': 0.02,
+ 'gamma2_err': 0.06,
+ 'p1_err': 0.18,
+ 'p2_err': 0.13,
+ 'p3_err': 0.62,
+ 'beta1_err': 0.09,
+ 'zstar_err': 0.08,
+ 'zstarc2_err': 0.10,
+ 'logLa_err': 10**0.07,
+ 'logLa2_err': 10**0.17,
+ 'alpha_err': 0.01,
+ 'alpha2_err': 0.02
 }
 
 qsolf_LDDE1_hardpars = \
@@ -143,18 +145,53 @@ qsolf_LDDE1_hardpars = \
 
 qsolf_LDDE1_harderr = \
 {
- 'K': 10**0.07,#???
- 'loglstar': 10**0.05,
- 'gamma1': 0.02,
- 'gamma2': 0.07,
- 'p1': 0.17,
- 'p2': 0.17,
- 'zstar': 0.09,
- 'logLa': 10**0.09,
- 'alpha': 0.01,
+ 'K_err': 10**0.07,#???
+ 'loglstar_err': 10**0.05,
+ 'gamma1_err': 0.02,
+ 'gamma2_err': 0.07,
+ 'p1_err': 0.17,
+ 'p2_err': 0.17,
+ 'zstar_err': 0.09,
+ 'logLa_err': 10**0.09,
+ 'alpha_err': 0.01,
  }
  
 #-------------------------------------------------
+
+def randomsamples(samples, K = None, loglstar = None, \
+gamma1 = None, gamma2 = None, p1 = None, p2  = None,\
+p3  = None, beta1 = None, zstar = None, zstarc2  = None, 
+logLa = None, logLa2 = None, alpha = None, alpha2 = None,\
+K_err = None, loglstar_err = None, gamma1_err = None, 
+gamma2_err = None, p1_err = None, p2_err = None, p3_err = None, \
+beta1_err = None, zstar_err = None, zstarc2_err = None,\
+logLa_err = None, logLa2_err = None, alpha_err = None, \
+alpha2_err = None, **kwargs):
+
+    randomsamples = []
+    for i in range(samples):
+        
+        randomsample = {
+        #'K': np.random.normal(K, K_err, samples),
+        'K': 10**-5.72,
+        'loglstar': np.random.normal(loglstar, loglstar_err, samples)[i],\
+        'gamma1': np.random.normal(gamma1, gamma1_err, samples)[i],\
+        'gamma2': np.random.normal(gamma2, gamma2_err, samples)[i],\
+        'p1': np.random.normal(p1, p1_err, samples)[i],\
+        'p2': np.random.normal(p2, p2_err, samples)[i],\
+        'p3': np.random.normal(p3, p3_err, samples)[i],\
+        'beta1': np.random.normal(beta1, beta1_err, samples)[i],\
+        'zstar': np.random.normal(zstar, zstar_err, samples)[i],\
+        'zstarc2': np.random.normal(zstarc2, zstarc2_err, samples)[i],\
+        'logLa': np.random.normal(logLa, logLa_err, samples)[i],\
+        'logLa2': np.random.normal(logLa2, logLa2_err, samples)[i],\
+        'alpha': np.random.normal(alpha, alpha_err, samples)[i],\
+        'alpha2': np.random.normal(alpha2, alpha2_err, samples)[i],\
+        }
+        randomsamples.append(randomsample)
+    return randomsamples
+  
+#-------------------------------------------------    
 
 def _LuminosityFunction_LDDE1(Lx, z, K = None, loglstar=None, A=None, gamma1=None, gamma2=None, p1=None, \
 p2 = None, beta1=None, zstar=None, logLa=None, alpha=None, **kwargs):
@@ -216,7 +253,7 @@ alpha = None, alpha2 = None):
         ex = (1+zc1)**p1*((1+zc2)/(1+zc1))**p2*((1+z)/(1+zc2))**p3
 
     p = (K * ((Lx / loglstar)**gamma1 + (Lx / loglstar)**gamma2)**-1 * ex)
-    print p
+
     return  K * ((Lx / loglstar)**gamma1 + (Lx / loglstar)**gamma2)**-1 * ex
     
 #-------------------------------------------------    
@@ -236,4 +273,105 @@ def LuminosityFunction_LDDE1_softband(Lx, z, **kwargs):
 def LuminosityFunction_LDDE2_softband(Lx, z, **kwargs):
 
     return _LuminosityFunction_LDDE2(Lx, z, **qsolf_LDDE2_softpars)
+       
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+"""
+J. Aird1, A. L. Coil, A. Georgakakis, K. Nandra, G. Barro, P. G. P´erez-Gonz´alez, 2015, ???, ???, ???
+
+There are two different models with two different sets of Data each:
+
+Model 1: LDDE1
+    -Simple model of Ueda 2003 (I Believe*** will check and fix**)
     
+Model 2: LDDE2:
+    -Complex model of UEDA 2014 (I  Believe** will check and fix**)
+    
+Data set 1:
+    -Soft band (0.5-2 KeV) parameters for both models 
+    
+Data set 2:
+    -Hard band (2-7 KeV) parameters for both models 
+    
+NOTE: 
+    
+    These models produce Lx(z) and does integrate and give the Lx density for either model.
+    
+    --- = spacing between different sections of code
+    
+    ### = spacing within a section of code to denote a different section within that particular code
+
+    I have the errors included in each dictionary. 
+"""
+
+#-------------------------------------------------
+
+#K is not using the random samples
+def _LuminosityFunction_LDDE1_integrate(Lx, z, K = None, loglstar = None, gamma1 = None, \
+gamma2 = None, p1 = None, p2 = None, zstar = None, logLa = None, alpha = None, **kawargs):
+    
+        
+    if Lx < logLa:
+        zc1 = zstar*(Lx / logLa)**alpha
+    elif Lx >= logLa:
+        zc1 = zstar
+    
+    #print zc1
+##################################################   
+
+    if z <= zc1:
+        ex = (1+z)**p1
+    elif z > zc1:
+        ex = (1+zc1)**p1*((1+z)/(1+zc1))**p2
+        
+##################################################  
+           
+    p = K * ((Lx / loglstar)**gamma1 + (Lx / loglstar)**gamma2)**-1 * ex     
+    return p
+   
+
+#-------------------------------------------------
+
+#K is not using the random samples
+def _LuminosityFunction_LDDE2_integrate(Lx, z, K = None, loglstar = None, gamma1 = None, gamma2 = None,\
+p1 = None, p2 = None, p3 = None, beta1 = None, zstar = None, zstarc2 = None, logLa = None, \
+logLa2 = None, alpha = None, alpha2 = None, **kwargs):
+    
+    """This equation is taken from Aird et al. (2015) and is based off of Ueda et al. (2014) LDDE equation. 
+    This equation stands from the Luminosity Dependent Desnity Equation (LDDE) used to describe the 
+    evolutoion of X-ray luminosity from AGNs on the basis of X-ray surveys. The X-ray luminosity Function 
+    (XLF) is best described using the LDDE model. The LDDE Model is a function of redshift and X-ray 
+    Luminosity best fitting within the range of 0-5 and 10**42-10**48, respectively. This model is the 
+    modified version of Ueda et al. (2003) LDDE model. This model accounts for the decay in the comoving 
+    numebr density of luminous AGN. """
+       
+    
+            
+    if Lx < logLa:
+        zc1 = zstar*(Lx / logLa)**alpha
+    elif Lx >= logLa:
+        zc1 = zstar
+    
+##################################################   
+    
+    if Lx < logLa2:
+        zc2 = zstarc2*(Lx / logLa2)**alpha2
+    elif Lx >= logLa2:
+        zc2 = zstarc2 
+    
+##################################################  
+      
+    if z < zc1:
+        ex = (1+z)**p1
+    elif zc1 < z < zc2:
+        ex = (1+zc1)**p1*((1+z)/(1+zc1))**p2
+    elif z > zc2:
+        ex = (1+zc1)**p1*((1+zc2)/(1+zc1))**p2*((1+z)/(1+zc2))**p3
+        
+##################################################  
+    
+    p = K * ((Lx / loglstar)**gamma1 + (Lx / loglstar)**gamma2)**-1 * ex
+  
+    return p
+    
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
--- tests/lit/test_ueda2014.py
+++ tests/lit/test_ueda2014.py
@@ -12,15 +12,15 @@ XLF and the redshift ranges arebinned, also my be cutting the plots off at a cer
 import ares
 import numpy as np
 import matplotlib.pyplot as pl
+from scipy import integrate
 
 u14 = ares.util.read_lit('ueda2014')
-
 z = np.linspace(2.4, 3.0, 100)
 L = np.logspace(42.5, 47, 100)
-
 fig1 = pl.figure(1); ax1 = fig1.add_subplot(111)
 
-colors = 'k', 'b', 'g', 'o' 'r', 'p'
+#---------------------------------------------------
+
 models = []
 r = []
 for i, redshift in enumerate(z):
@@ -39,6 +39,105 @@ for i, j in enumerate(models):
     pl.ylim(10**-12, 10**-2)
     pl.xlim(L[0], L[-23])
 
+ax1.set_xlabel(r'$L_X$')    
+ax1.set_ylabel(r'$\phi(L_X)$')
+ax1.legend(loc='best')
+pl.show()
+
+#---------------------------------------------------
+
+"""
+test_Lx(z)
+
+Author: Jacob Jost
+Affiliation: University of Colorado at Boulder (Undergraduate)
+Created on: Thu June 4 09:00:00 MDT 2015
+
+NOTE: 
+    plots may differ than Ueda et al. (2014) because their plots show the De-absorbed
+    XLF and the redshift ranges arebinned, also my be cutting the plots off at a certain value***
+
+    HEADS UP: this takes a while to run, use caution. 
+"""
+
+#---------------------------------------------------
+
+u14 = ares.util.read_lit('ueda2014')
+z = np.linspace(0, 5, 100)
+models = 100
+
+
+hardpars = u14.qsolf_LDDE2_hardpars #parameters dictionary
+harderr = u14.qsolf_LDDE2_harderr #parameters error dictionary
+hardall = hardpars.copy() #copying parameters dictionary
+hardall.update(harderr) #combining parameters dictionary & parameters error dictionary
+hardsamples = u14.randomsamples(100, **hardall)
+
+fig2 = pl.figure(2); ax2 = fig2.add_subplot(111)
+
+integrand = []
+for i in range(models):
+    integrand1 = []
+    r = []
+    integrand.append(integrand1)
+    for j in range(len(z)):
+        x = lambda Lx: u14._LuminosityFunction_LDDE(Lx, z[j],\
+        **hardsamples[i])
+        p, err = integrate.quad(x, 10**41, 10**46)
+        r.append(z[j])
+        integrand1.append(p)
+          
+for i, j in enumerate(integrand):
+    pl.semilogy(z, integrand[i], color = 'k', alpha = 0.25)
+
+pl.title(r'The 2-10 KeV Band at $z$ ~ $%.1f - %.1f$' % (r[0], r[-1]))
+
+ax2.set_xlabel(r'$z$')    
+ax2.set_ylabel(r'$\phi(L_X)$')
+ax2.legend(loc='best')
+pl.show()
+
+#---------------------------------------------------
+
+"""
+test_Lx(z)
+
+Author: Jacob Jost
+Affiliation: University of Colorado at Boulder (Undergraduate)
+Created on: Thu June 4 09:00:00 MDT 2015
+
+NOTE: plots may differ than Ueda et al. (2014) because their plots show the De-absorbed
+XLF and the redshift ranges arebinned, also my be cutting the plots off at a certain value***
+"""
+
+z = np.linspace(2.4, 3.0, 100)
+L = np.logspace(42.5, 47, 100)
+fig3 = pl.figure(3); ax3 = fig3.add_subplot(111)
+
+hardpars = u14.qsolf_LDDE2_hardpars #parameters dictionary
+harderr = u14.qsolf_LDDE2_harderr #parameters error dictionary
+hardall = hardpars.copy() #copying parameters dictionary
+hardall.update(harderr) #combining parameters dictionary & parameters error dictionary
+hardsamples = u14.randomsamples(100, **hardall)
+
+results = []
+r = []
+for i, redshift in enumerate(z):
+    model1 = []
+    results.append(model1)
+    r.append(redshift)
+    for p, Lx in enumerate(L):
+        model = u14._LuminosityFunction_LDDE(Lx, redshift,\
+        **hardsamples[i])
+        model1.append(model)
+
+          
+for i, j in enumerate(results):
+    pl.loglog(L, results[i], color = 'r', alpha = 0.25)
+
+pl.title(r'The 2-10 KeV Band at $z$ ~ $%.1f - %.1f$' % (r[0], r[-1]))
+pl.ylim(10**-12, 10**-4)
+pl.xlim(L[0], L[-23])
 ax1.set_xlabel(r'$L_X$')    
 ax1.set_ylabel(r'$\phi(L_X)$')
 ax1.legend(loc='best')
