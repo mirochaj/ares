found the missing factor of eta in HAM model. other random fixes

ares/analysis/InlineAnalysis.py
@@ -384,7 +384,7 @@ class InlineAnalysis:
                 
             try:
                 sfrd = pop.SFRD(z) * rhodot_cgs
-            except SystemExit:
+            except SystemExit,ValueError:
                 sfrd = np.inf    
             
             return sfrd

ares/inference/FitGLF.py
@@ -183,7 +183,7 @@ class loglikelihood:
                 save[par] = 10**pars[i]
             else:
                 save[par] = pars[i]
-    
+            
         # Apply prior on model parameters first (dont need to generate signal)
         lp = self.logprior_P(pars)
         if not np.isfinite(lp):
@@ -201,7 +201,7 @@ class loglikelihood:
             sim.run()
             t2 = time.time()
             sim.run_inline_analysis()
-                        
+                                    
             tfn = '%s.timing_%s.pkl' % (self.prefix, str(rank).zfill(4))
             with open(tfn, 'ab') as f:
                 pickle.dump((t2 - t1, kwargs), f)
@@ -512,7 +512,7 @@ class FitGLF(object):
         f.close()
      
     def run(self, prefix, steps=1e2, burn=0, clobber=False, restart=False, 
-        runsim=False, save_freq=500):
+        runsim=False, save_freq=500, live_dangerously=False):
         """
         Run MCMC.
     
@@ -608,9 +608,13 @@ class FitGLF(object):
             if pars != self.parameters:
                 if size > 1:
                     if rank == 0:
-                        print 'parameters from file dont match those supplied!'
-                    MPI.COMM_WORLD.Abort()
-                raise ValueError('parameters from file dont match those supplied!')
+                        print 'warning: parameters from file dont match those supplied!'
+                    
+                    if not live_dangerously:
+                        if rank == 0:
+                            print "if you dont care, set live_dangerously=True"
+                        MPI.COMM_WORLD.Abort()
+                        raise ValueError('parameters from file dont match those supplied!')
             if is_log != self.is_log:
                 if size > 1:
                     if rank == 0:
@@ -635,9 +639,12 @@ class FitGLF(object):
             if ct > 0:
                 if size > 1:
                     if rank == 0:
-                        print 'base_kwargs from file dont match those supplied!'
-                    MPI.COMM_WORLD.Abort()
-                raise ValueError('base_kwargs from file dont match those supplied!')            
+                        print 'warning: base_kwargs from file dont match those supplied!'
+                    if not live_dangerously:
+                        if rank == 0:
+                            print "if you dont care, set live_dangerously=True"
+                        MPI.COMM_WORLD.Abort()
+                        raise ValueError('base_kwargs from file dont match those supplied!')            
     
             # Start from last step in pre-restart calculation
             chain = read_pickled_chain('%s.chain.pkl' % prefix)

ares/populations/Galaxy.py
@@ -325,11 +325,14 @@ class GalaxyPopulation(HaloPopulation):
 
                 integ = self.halos.dndlnm[i] * Macc
 
+                p0 = simps(integ[j1-1:], x=self.halos.lnM[j1-1:])
                 p1 = simps(integ[j1:], x=self.halos.lnM[j1:])
                 p2 = simps(integ[j2:], x=self.halos.lnM[j2:])
+                p3 = simps(integ[j2+1:], x=self.halos.lnM[j2+1:])
                 
-                lhs = np.interp(np.log(Mmin), self.halos.lnM[j1:j1+2],
-                    [p1, p2])
+                interp = interp1d(self.halos.lnM[j1-1:j1+3], [p0,p1,p2,p3])
+                
+                lhs = interp(np.log(Mmin))
                     
                 self._eta[i] = rhs / lhs
 
@@ -394,9 +397,7 @@ class GalaxyPopulation(HaloPopulation):
             alpha = self.constraints['alpha'][i]
             L_star = self.constraints['Lstar'][i]
             phi_star = self.constraints['pstar'][i]
-        
-            #self.halos.MF.update(z=z)
-            
+                    
             eta = self.eta[i]
             ngtm = self.halos.ngtm[np.argmin(np.abs(z - self.halos.z))]
         
@@ -500,7 +501,7 @@ class GalaxyPopulation(HaloPopulation):
         if not hasattr(self, '_sfr_ham_'):
             self._sfr_ham_ = np.zeros([self.halos.Nz, self.halos.Nm])
             for i, z in enumerate(self.halos.z):
-                self._sfr_ham_[i] = self.cosm.fbaryon \
+                self._sfr_ham_[i] = self.eta[i] * self.cosm.fbaryon \
                     * self.Macc(z, self.halos.M) * self.fstar(z, self.halos.M)
             
         return self._sfr_ham_
@@ -903,4 +904,3 @@ class GalaxyPopulation(HaloPopulation):
         
 
               
-        

tests/populations/test_ham.py
@@ -22,14 +22,11 @@ lf_pars['z'] = b15.redshifts
 
 pars = \
 {
- 'pop_fstar_ceil': 0.3,
  'pop_model': 'ham',
  'pop_Macc': 'mcbride2009',
  'pop_constraints': lf_pars,  # bouwens2015
  'pop_kappa_UV': 1.15e-28,
  'pop_logM': np.arange(10, 13.5, 0.25), # Masses for AM
- 'hmf_func': 'PS',
- 'hmf_analytic': False,
 }
 
 pop = ares.populations.GalaxyPopulation(**pars)
@@ -170,7 +167,7 @@ ax8.legend(loc='lower right', fontsize=14)
 # eta vs z
 fig9 = pl.figure(9); ax9 = fig9.add_subplot(111)
 
-pl.semilogy(pop.halos.z, pop.eta)
+ax9.plot(pop.halos.z, pop.eta)
 ax9.set_xlabel(r'$z$')
 ax9.set_ylabel(r'$\eta(z)$')
 