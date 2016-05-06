efe1c2eaa0afd047953ed7020e2e1571f9e0fd62
Jordan Mirocha <mirochaj@gmail.com>
2016-05-06 12:22:34 -0700

    added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ setup(name='ares',
       author_email='mirochaj@gmail.com',
       url=ares_link,
       packages=ares_packages,
-      #entry_points={'console_scripts': [
-      #'ares = ares.util.CommandLine:run_main',
-      #]},
+      install_requires=[
+        'numpy',
+      ],
      )
           
 # Try to set up $HOME/.ares: this won't work on WINDOWS at the moment
