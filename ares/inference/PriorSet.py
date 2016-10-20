453612c98e11a14987d95a3ed8141ef06576d79e
ktausch <devnull@localhost>
2016-10-19 20:49:30 -0600

    Added delete_prior function to PriorSet

--- ares/inference/PriorSet.py
+++ ares/inference/PriorSet.py
@@ -293,6 +293,32 @@ class PriorSet(object):
         raise ValueError(("The parameter searched for (%s)" % (parameter,)) +\
                          "in a PriorSet was not found.")
 
+    def delete_prior(self, parameter, throw_error=True):
+        """
+        Deletes a prior from this PriorSet.
+        
+        parameter a parameter in the prior
+        throw_error if True (default), an error is thrown if the parameter
+                    is not found
+        """
+        broken = False
+        for iprior in range(len(self._data)):
+            (this_prior, these_params, these_transforms) = self._data[iprior]
+            for iparam in range(len(these_params)):
+                if parameter == these_params[iparam]:
+                    to_delete = iprior
+                    broken = True
+                    break
+            if broken:
+                break
+        try:
+            self._data = self._data[:to_delete] + self._data[to_delete + 1:]
+        except:
+            if throw_error:
+                raise ValueError('The parameter given to ' +\
+                                 'PriorSet.delete_prior was not in ' +\
+                                 'the PriorSet.')
+
     def parameter_strings(self, parameter):
         """
         Makes an informative string about this parameter's place in this
