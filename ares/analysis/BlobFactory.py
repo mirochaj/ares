important bug fix: secondary_ionization method 3 sign error in higher order terms

ares/analysis/BlobFactory.py
@@ -421,9 +421,7 @@ class BlobFactory(object):
                             blob = np.array(map(func, x))
                         else:
                             blob = np.interp(x, func[0], func[1])
-                            
-                        print key, type(blob), blob.shape
-                                    
+                                                                
                 else:
                     # Must have blob_funcs for this case
                     fname = self.blob_funcs[i][j]

ares/analysis/ModelSet.py
@@ -821,6 +821,8 @@ class ModelSet(BlobFactory):
         self._ax.add_patch(rect)
         self._ax.figure.canvas.draw()
         
+        print (lx, lx+dx, ly, ly+dy)
+        
         self.Slice((lx, lx+dx, ly, ly+dy), **self.plot_info)
                 
     def Slice(self, constraints, pars, ivar=None, take_log=False, 
@@ -1197,14 +1199,14 @@ class ModelSet(BlobFactory):
         
     def LinePlot(self, pars, ivar=None, ax=None, fig=1, c=None,
         take_log=False, un_log=False, multiplier=1., use_colorbar=True, 
-        **kwargs):
+        sort=True, **kwargs):
         return self.Scatter(pars, ivar, ax=ax, fig=fig, c=c,
             take_log=take_log, un_log=un_log, multiplier=multiplier, 
-            use_colorbar=use_colorbar, line_plot=True, **kwargs)
+            use_colorbar=use_colorbar, line_plot=True, sort=sort, **kwargs)
 
     def Scatter(self, pars, ivar=None, ax=None, fig=1, c=None,
         take_log=False, un_log=False, multiplier=1., use_colorbar=True, 
-        line_plot=False, **kwargs):
+        line_plot=False, sort=True, **kwargs):
         """
         Plot samples as points in 2-d plane.
     
@@ -1260,11 +1262,12 @@ class ModelSet(BlobFactory):
             
         if line_plot:
             # The ordering of the points doesn't matter
-            order = np.argsort(xdata)
-            xdata = xdata[order]
-            ydata = ydata[order]
-            if cdata is not None:
-                cdata = cdata[order]
+            if sort:
+                order = np.argsort(xdata)
+                xdata = xdata[order]
+                ydata = ydata[order]
+                if cdata is not None:
+                    cdata = cdata[order]
                         
             func = ax.__getattribute__('plot')
         else:
@@ -1284,6 +1287,7 @@ class ModelSet(BlobFactory):
         
         self._scat = scat
             
+        # Might use this for slicing 
         self.plot_info = {'pars': pars, 'ivar': ivar,
             'take_log': take_log, 'un_log':un_log, 'multiplier':multiplier}
             

ares/physics/Hydrogen.py
@@ -21,6 +21,10 @@ from .Constants import A10, T_star, m_p, m_e, erg_per_ev, h, c, E_LyA, E_LL, \
 
 try:
     from scipy.special import gamma
+    g23 = gamma(2. / 3.)
+    g13 = gamma(1. / 3.)
+    c1 = 4. * np.pi / 3. / np.sqrt(3.) / g23
+    c2 = 8. * np.pi / 3. / np.sqrt(3.) / g13
 except ImportError:
     pass
 
@@ -56,12 +60,6 @@ T_He = np.array(T_He)
 
 l_LyA = h * c / E_LyA / erg_per_ev
 
-g23 = gamma(2. / 3.)
-g13 = gamma(1. / 3.)
-
-c1 = 4. * np.pi / 3. / np.sqrt(3.) / g23
-c2 = 8. * np.pi / 3. / np.sqrt(3.) / g13
-
 class Hydrogen(object):
     def __init__(self, cosm=None, **kwargs):
         
@@ -77,25 +75,7 @@ class Hydrogen(object):
         self.approx_S = self.pf['approx_Salpha']
         
         self.nmax = self.pf['lya_nmax']
-        self.fbarII = 0.72
-        self.fbarIII = 0.63
-        self.A10 = 2.85e-15 			
-        self.E10 = 5.9e-6 				
-        self.m_H = m_p + m_e     		
-        self.nu_0 = 1420.4057e6 			
-        self.T_star = 0.068 				
-        self.a_0 = 5.292e-9 				
-                
-        # Common lines, etc.
-        #self.nu_LL = 13.6 * erg_per_ev / h
-        #self.E_LyA = h * c / (1216. * 1e2 / 1e10) / erg_per_ev
-        #self.E_LyB = h * c / (1026. * 1e2 / 1e10) / erg_per_ev
-        #self.E_LL = h * self.nu_LL / erg_per_ev
-        #
-        #self.nu_alpha = self.E_LyA * erg_per_ev / h
-        #self.nu_beta = self.E_LyB * erg_per_ev / h
-        #self.dnu = self.nu_LL - self.nu_alpha
-        
+
         self.tabulated_coeff = \
             {'kappa_H': kappa_HH, 'kappa_e': kappa_He, 
              'T_H': T_HH, 'T_e': T_He}
@@ -290,18 +270,18 @@ class Hydrogen(object):
         """
                 
         return 1.81e11 * self.Sa(z=z, Tk=Tk, xHII=xHII) * Ja / (1. + z)
-        
+
     def tauGP(self, z, xHII=0.):
         """ Gunn-Peterson optical depth. """
         return 1.5 * self.cosm.nH(z) * (1. - xHII) * l_LyA**3 * 50e6 \
             / self.cosm.HubbleParameter(z)
-    
+
     def lya_width(self, Tk):
         """
         Returns Doppler line-width of the Ly-a line in eV.
         """
         return np.sqrt(2. * k_B * Tk / m_e / c**2) * E_LyA
-        
+
     def Sa(self, z=None, Tk=None, xHII=0.0):
         """
         Account for line profile effects.
@@ -315,9 +295,10 @@ class Hydrogen(object):
             return np.exp(-0.37 * np.sqrt(1. + z) * Tk**(-2./3.)) \
                 / (1. + 0.4 / Tk)
         elif self.approx_S == 3:
-            gamma = 1. / self.tauGP(z, xHII=xHII) / (1. + 0.4 / Tk)
-            alpha = 0.717 * Tk**(-2./3.) * (1e-6 / gamma)**(1. / 3.)
-            return 1. - c1 * alpha - c2 * alpha**2 + 4. * alpha**3 / 3.
+            gamma = 1. / self.tauGP(z, xHII=xHII) / (1. + 0.4 / Tk)  # Eq. 4
+            alpha = 0.717 * Tk**(-2./3.) * (1e-6 / gamma)**(1. / 3.) # Eq. 20
+            S = 1. - c1 * alpha + c2 * alpha**2 - 4. * alpha**3 / 3. # Eq. 19
+            return S
         else:
             raise NotImplementedError('approx_Sa must be in [1,2,3].')
             

input/litdata/eldridge2009.py
@@ -98,7 +98,8 @@ def _load(**kwargs):
         
         _raw_data = np.zeros_like(d1)
         for i, t in enumerate(times):
-            inter = interp1d(np.log10(Zv), to_interp[:,:,i], axis=0)
+            inter = interp1d(np.log10(Zv), to_interp[:,:,i], axis=0,
+                kind='linear')
             _raw_data[:,i] = inter(np.log10(kwargs['pop_Z']))
 
         data = 10**_raw_data

tests/test_physics_HI_wf.py
@@ -15,24 +15,24 @@ import numpy as np
 import matplotlib.pyplot as pl
 
 def test():
-    zarr = np.linspace(20, 40)
+    zarr = np.linspace(5, 40)
     
     hydr1 = ares.physics.Hydrogen(approx_Salpha=1)
     hydr2 = ares.physics.Hydrogen(approx_Salpha=2)
     hydr3 = ares.physics.Hydrogen(approx_Salpha=3)
     
-    pl.plot(zarr, map(lambda z: hydr1.Sa(z, hydr1.cosm.Tgas(z)), zarr), 
+    pl.plot(zarr, map(lambda z: 1. - hydr1.Sa(z, hydr1.cosm.Tgas(z)), zarr), 
         color='k', ls='-')
-    pl.plot(zarr, map(lambda z: hydr2.Sa(z, hydr2.cosm.Tgas(z)), zarr), 
+    pl.plot(zarr, map(lambda z: 1. - hydr2.Sa(z, hydr2.cosm.Tgas(z)), zarr), 
         color='k', ls='--', label='Chuzhoy, Alvarez, & Shapiro \'05')
-    pl.plot(zarr, map(lambda z: hydr3.Sa(z, hydr3.cosm.Tgas(z)), zarr), 
+    pl.plot(zarr, map(lambda z: 1. - hydr3.Sa(z, hydr3.cosm.Tgas(z)), zarr), 
         color='k', ls=':', label='Furlanetto & Pritchard \'06')   
          
     pl.ylim(0, 1.1)
     pl.xlabel(r'$z$')
-    pl.ylabel(r'$S_{\alpha}$')
-    pl.legend(loc='lower right', frameon=False, fontsize=14)
-    pl.annotate(r'assuming $T_k \propto (1+z)^2$', (0.1, 0.4), 
+    pl.ylabel(r'$1-S_{\alpha}$')
+    pl.legend(loc='upper right', frameon=False, fontsize=14)
+    pl.annotate(r'assuming $T_k \propto (1+z)^2$', (0.05, 0.05), 
         xycoords='axes fraction')
         
     pl.savefig('%s.png' % (__file__.rstrip('.py')))