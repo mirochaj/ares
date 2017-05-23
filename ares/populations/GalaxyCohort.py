fixed bug in non-tabular sed normalization through GalaxyCohort.__getattr__

ares/populations/GalaxyCohort.py
@@ -17,7 +17,6 @@ from ..util import read_lit
 from inspect import ismethod
 from scipy.optimize import fsolve, minimize
 from types import FunctionType, InstanceType
-from .GalaxyAggregate import GalaxyAggregate
 from ..analysis.BlobFactory import BlobFactory
 from ..util import MagnitudeSystem, ProgressBar
 from ..phenom.DustCorrection import DustCorrection
@@ -25,6 +24,7 @@ from scipy.integrate import quad, simps, cumtrapz, ode
 from ..util.ParameterFile import par_info, get_pq_pars
 from ..physics.RateCoefficients import RateCoefficients
 from scipy.interpolate import interp1d, RectBivariateSpline
+from .GalaxyAggregate import GalaxyAggregate, normalize_sed
 from ..util.Math import central_difference, interp1d_wrapper
 from ..phenom.ParameterizedQuantity import ParameterizedQuantity
 from ..physics.Constants import s_per_yr, g_per_msun, cm_per_mpc, G, m_p, \
@@ -80,7 +80,7 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
                     
         # This is the name of the thing as it appears in the parameter file.
         full_name = 'pop_' + name
-                
+                                
         # Now, possibly make an attribute
         if not hasattr(self, name):
             
@@ -145,13 +145,19 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
                 self._update_pq_registry(name, result)
             
             elif type(self.pf[full_name]) in [int, float, np.float64]:
-                result = lambda **kwargs: self.pf[full_name]
-
+                
+                # Need to be careful here: has user-specified units!
+                # We've assumed that this cannot be parameterized...
+                # i.e., previous elif won't ever catch rad_yield
+                if name == 'rad_yield':
+                    result = lambda **kwargs: normalize_sed(self)
+                else:
+                    result = lambda **kwargs: self.pf[full_name]
+                
             else:
                 raise TypeError('dunno how to handle: %s' % name)
 
             # Check to see if Z?
-
             self.__setattr__(name, result)
 
         return getattr(self, name)
@@ -1669,6 +1675,7 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
     def yield_per_sfr(self):
         # Need this to avoid inheritance issue with GalaxyAggregate
         if not hasattr(self, '_yield_per_sfr'):
+                        
             if type(self.rad_yield) is FunctionType:
                 self._yield_per_sfr = self.rad_yield()
             else:
@@ -1798,6 +1805,10 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
         
         fb = self.cosm.fbar_over_fcdm
         
+        # Splitting up the inflow. P = pristine, 
+        PIR = -1. * fb * self.MAR(z, Mh) * self.cosm.dtdz(z) / s_per_yr
+        NPIR = -1. * fb * self.MDR(z, Mh) * self.cosm.dtdz(z) / s_per_yr
+        
         # Measured relative to baryonic inflow
         Mb = fb * Mh
         Zfrac = self.pf['pop_acc_frac_metals'] * (MZ / Mb)
@@ -1816,10 +1827,6 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
 
         # Eq. 1: halo mass.
         y1p = -1. * self.MGR(z, Mh) * self.cosm.dtdz(z) / s_per_yr
-
-        # Splitting up the inflow. P = pristine, 
-        PIR = -1. * fb * self.MAR(z, Mh) * self.cosm.dtdz(z) / s_per_yr
-        NPIR = -1. * fb * self.MDR(z, Mh) * self.cosm.dtdz(z) / s_per_yr
 
         # Eq. 2: gas mass
         if self.pf['pop_sfr'] is None:
@@ -1901,6 +1908,11 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
     @property
     def constant_SFE(self):
         if not hasattr(self, '_constant_SFE'):
+            
+            if self.constant_SFR:
+                self._constant_SFE = 0
+                return self._constant_SFE
+            
             self._constant_SFE = 1
             for mass in [1e7, 1e8, 1e9, 1e10, 1e11, 1e12]:
                 self._constant_SFE *= self.fstar(z=10, Mh=mass) \

ares/simulations/MetaGalacticBackground.py
@@ -592,7 +592,7 @@ class MetaGalacticBackground(AnalyzeMGB):
         return self._z_unique        
                 
     def get_uvb_tot(self, include_pops=None):
-        
+                
         if include_pops is None:
             include_pops = range(self.solver.Npops)
         
@@ -601,7 +601,7 @@ class MetaGalacticBackground(AnalyzeMGB):
         _f_Ja = []
         _f_Jlw = []
         for i, popid in enumerate(include_pops):
-            
+                        
             _z, _Ja, _Jlw = self.get_uvb(popid)
             
             _allz.append(_z)
@@ -654,6 +654,7 @@ class MetaGalacticBackground(AnalyzeMGB):
         # Need better long-term fix: Lya sources aren't necessarily LW 
         # sources, if (for example) approx_all_pops = True. 
         if not self.pf['feedback_LW']:
+            # Will use all then
             include_pops = None
         elif include_pops is None:
             include_pops = range(self.solver.Npops)
@@ -740,7 +741,7 @@ class MetaGalacticBackground(AnalyzeMGB):
             return True 
         
         rtol = self.pf['feedback_LW_Mmin_rtol']
-        atol = self.pf['feedback_LW_Mmin_atol']      
+        atol = self.pf['feedback_LW_Mmin_atol']
         
         # Less stringent requirement, that mean error meet tolerance.
         if self.pf['feedback_LW_mean_err']:
@@ -783,43 +784,47 @@ class MetaGalacticBackground(AnalyzeMGB):
         if np.any(self.solver.solve_rte[popid]):
             z, E, flux = self.get_history(popid=popid, flatten=True)
             
-            l = np.argmin(np.abs(E - E_LyA))     # should be 0
-                                                            
-            # Redshift is first dimension!
-            Ja = flux[:,l]
-                    
-            # Find photons in LW band    
+            if self.pf['secondary_lya'] and self.pops[popid].is_ion_src_igm:
+                Ja = np.zeros_like(z) # placeholder
+            else:
+                l = np.argmin(np.abs(E - E_LyA))     # should be 0
+                                                                
+                # Redshift is first dimension!
+                Ja = flux[:,l]
+                        
+            # Find photons in LW band
             is_LW = np.logical_and(E >= 11.18, E <= E_LL)
             
             dnu = (E_LL - 11.18) / ev_per_hz
-
+            
             # Need to do an integral to finish this one.
             Jlw = np.zeros_like(z)
-        
+
         else:
             # Need a redshift array!
             z = self.z_unique
             Ja = np.zeros_like(z)
             Jlw = np.zeros_like(z)
-        
+
         ##
         # Loop over redshift
         ##
-        for i, redshift in enumerate(z): 
-            
+        for i, redshift in enumerate(z):
             if not np.any(self.solver.solve_rte[popid]):
                 Ja[i] = self.solver.LymanAlphaFlux(redshift, popid=popid)
-                if self.pf['feedback_LW']:   
+                if self.pf['feedback_LW']:
                     Jlw[i] = self.solver.LymanWernerFlux(redshift, popid=popid)
+
                 continue
-                       
-            LW_flux = flux[i,is_LW]
-            
+            elif self.pf['secondary_lya'] and (self.pops[popid].is_ion_src_igm):
+                Ja[i] = self.solver.volume.SecondaryLymanAlphaFlux(redshift, 
+                    popid=popid, fluxes={popid:flux[i]})
+
             # Convert to energy units, and per eV to prep for integral
-            LW_flux *= E[is_LW] * erg_per_ev / ev_per_hz
-            
+            LW_flux = flux[i,is_LW] * E[is_LW] * erg_per_ev / ev_per_hz
+
             Jlw[i] = np.trapz(LW_flux, x=E[is_LW]) / dnu
-        
+
         return z, Ja, Jlw
 
     def get_history(self, popid=0, flatten=False):

ares/util/SetDefaultParameterValues.py
@@ -183,7 +183,7 @@ def PhysicsParameters():
     "radiative_transfer": 1,
     "photon_conserving": 1, 
     "plane_parallel": 0,   
-    "infinite_c": 1,  
+    "infinite_c": 1,
     
     "collisional_ionization": 1,
     
@@ -344,8 +344,6 @@ def PopulationParameters():
     "pop_sfr_above_threshold": True,
     "pop_sfr_cross_threshold": True,
     "pop_sfr_cross_upto_Tmin": np.inf,
-
-    "pop_feels_feedback": True,
         
     # Mass accretion rate
     "pop_MAR": 'hmf',