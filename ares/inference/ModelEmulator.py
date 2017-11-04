8592c5db861e59ce23e46e60cfd11767dd63fc25
Jordan Mirocha <mirochaj@gmail.com>
2017-11-04 14:36:53 -0700

    minor addition to cosmology test problem

--- ares/inference/ModelEmulator.py
+++ ares/inference/ModelEmulator.py
@@ -83,8 +83,10 @@ class ModelEmulator(object):
         """
         
         Ns = self.tset.chain.shape[0]
-        if downsample:
-            Ns = min(Ns, downsample)
+        
+        if downsample is not None:
+            Ns = min(Ns, int(downsample))
+            
         sample_ids = np.arange(0, Ns)
         
         # Set independent variables for training
