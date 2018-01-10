fixed sign error in new loglikelihood wrapper

ares/inference/FitGalaxyPopulation.py
@@ -154,7 +154,7 @@ class loglikelihood(LogLikelihood):
         
         del pops
 
-        lnL = 0.5 * np.ma.sum((phi - self.ydata)**2 / self.error**2)
+        lnL = -0.5 * np.ma.sum((phi - self.ydata)**2 / self.error**2)
 
         return lnL + self.const_term
     

ares/inference/FitGlobal21cm.py
@@ -81,11 +81,17 @@ class loglikelihood(LogLikelihood):
                 sim.history['igm_dTb'])
 
         if np.any(np.isnan(yarr)):
+            
+            #import matplotlib.pyplot as pl
+            #pl.plot(self.xdata, yarr)
+            #print('hey! GS: lnL={}'.format(lnL))
+            #raw_input('<enter>')
+            
             return -np.inf
 
-        lnL = 0.5 * (np.sum((yarr - self.ydata)**2 \
+        lnL = -0.5 * (np.sum((yarr - self.ydata)**2 \
             / self.error**2 + np.log(2. * np.pi * self.error**2))) 
-
+            
         return lnL + self.const_term
 
 class FitGlobal21cm(FitBase):

ares/inference/ModelFit.py
@@ -1032,7 +1032,11 @@ class ModelFit(FitBase):
             iterations=steps, rstate0=state, storechain=False):
             
             # Only the rank 0 processor ever makes it here
-                        
+            
+            print(pos.shape)
+            print(pos)
+            print(prob)
+                                    
             # If we're saving each checkpoint to its own file, this is the
             # identifier to use in the filename
             dd = 'dd' + str(ct // save_freq).zfill(4)
@@ -1244,12 +1248,10 @@ class ModelFit(FitBase):
         lnL = 0.0
         for fitter in self.fitters:
             lnL += fitter.loglikelihood(sim)
-    
+                
         # Final posterior calculation
-        PofD = lp - lnL
-    
-        print(lp, lnL)
-    
+        PofD = lp + lnL
+        
         if np.isnan(PofD):
             del sim, kw, kwargs
             gc.collect()