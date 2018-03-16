slight fix in radio BG calculation to accommodate new interp1d wrapper

ares/simulations/Global21cm.py
@@ -337,7 +337,8 @@ class Global21cm(AnalyzeGlobal21cm):
             z, E, flux = self.field.get_history(popid, flatten=True)
 
             E21cm = h_p * nu_0_mhz * 1e6 / erg_per_ev
-            f21 = interp1d(E, flux, axis=1, bounds_error=False, fill_value=0.0)
+            f21 = interp1d(E, flux, axis=1, bounds_error=False, 
+                fill_value=0.0, force_scipy=True)
             flux_21cm = f21(E21cm)
 
             Tr += np.interp(self.history['z'], z, flux_21cm) \

ares/util/Math.py
@@ -15,10 +15,10 @@ from ..physics.Constants import nu_0_mhz
 from scipy.interpolate import interp1d as interp1d_scipy
 
 def interp1d(x, y, kind='linear', fill_value=0.0, bounds_error=False,
-    **kwargs):
-    if kind == 'linear':
+    force_scipy=False, **kwargs):
+    if (kind == 'linear') and (not force_scipy):
         return lambda xx: np.interp(xx, x, y)
-    elif kind == 'cubic': 
+    elif (kind == 'cubic') or force_scipy: 
         return interp1d_scipy(x, y, kind='cubic', bounds_error=bounds_error, 
             fill_value=fill_value, **kwargs)
     else: