bug fix in LF interpolation, only applies for blob generation after calculation. also type error in PQ floor

ares/analysis/Global21cm.py
@@ -357,10 +357,10 @@ class Global21cm(MultiPhaseMedium,BlobFactory):
         ax.fill_between(self.history['nu'], dTb, 500 * np.ones_like(dTb),
             color='none', hatch='X', edgecolor='k', linewidth=0.0)
 
-        ax.set_xlim(xlim)    
+        ax.set_xlim(xlim)
         ax.set_ylim(ylim)
         pl.draw()
-        
+
         return ax
         
     def AdiabaticFloor(self, ax, gap=None, temp_units='mk', **kwargs):
@@ -371,24 +371,27 @@ class Global21cm(MultiPhaseMedium,BlobFactory):
             conv = 1e-3
         else:
             conv = 1.
-        
+
         xlim = ax.get_xlim()
         ylim = ax.get_ylim()
-        
+
+        nu = self.history['nu']
+        dTb = dTb
+
         if kwargs == {}:
-            ax.fill_between(self.history['nu'], 
-                -500 * np.ones_like(dTb) * conv, dTb * conv, 
+            ax.fill_between(nu, 
+                -2e3 * np.ones_like(dTb) * conv, dTb * conv, 
                 color='none', hatch='X', edgecolor='k', linewidth=0.0)
         else:
             if gap is None:
-                ax.plot(self.history['nu'], dTb * conv, **kwargs)
+                ax.plot(nu, dTb * conv, **kwargs)
             else:
-                i1 = np.argmin(np.abs(self.history['nu'] - gap[0]))
-                i2 = np.argmin(np.abs(self.history['nu'] - gap[1]))
+                i1 = np.argmin(np.abs(nu - gap[0]))
+                i2 = np.argmin(np.abs(nu - gap[1]))
                 
-                ax.plot(self.history['nu'][0:i1], dTb[0:i1] * conv, **kwargs)
-                ax.plot(self.history['nu'][i2:], dTb[i2:] * conv, **kwargs)
-            
+                ax.plot(nu[0:i1], dTb[0:i1] * conv, **kwargs)
+                ax.plot(nu[i2:], dTb[i2:] * conv, **kwargs)
+
         ax.set_xlim(xlim)    
         ax.set_ylim(ylim)
             

ares/analysis/MultiPhaseMedium.py
@@ -897,7 +897,7 @@ def add_redshift_axis(ax, twin_ax=None, zlim=80):
     fig = ax.xaxis.get_figure()
     
     if zlim > 100:
-        
+
         z = np.array([20, 40, 100, 400])[-1::-1]
         #z = np.arange(20, zlim, 40)[-1::-1]
         z_minor = np.arange(50, zlim, 50)[-1::-1]
@@ -907,11 +907,8 @@ def add_redshift_axis(ax, twin_ax=None, zlim=80):
         z_minor = np.arange(20, zlim, 10)[-1::-1]
         highz_labels = ['30', '80']
         
-    nu = nu_0_mhz / (1. + z)
-    nu_minor = nu_0_mhz / (1. + z_minor)
-    
     #z_labels = list(map(str, z))
-    lowz_labels = map(str, [6, 8, 10, 12, 15])
+    lowz_labels = map(str, [6, 8, 10, 12, 15, 20])
 
     z_labels = lowz_labels + highz_labels
     z = np.array(list(map(int, z_labels)))

ares/phenom/ParameterizedQuantity.py
@@ -375,7 +375,7 @@ class ParameterizedQuantity(object):
         if self.ceil is not None:
             f = np.minimum(f, self.ceil)
         if self.floor is not None:
-            if type(self.floor) in [int, float]:
+            if type(self.floor) in [int, float, np.float64]:
                 f = np.maximum(f, self.floor)
             else:
                 f = np.maximum(f, self.floor(**kwargs))