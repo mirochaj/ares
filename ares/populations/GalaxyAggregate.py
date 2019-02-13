bugfix for sfrd-tab option when using internal sfrd units

ares/populations/GalaxyAggregate.py
@@ -75,8 +75,12 @@ class GalaxyAggregate(HaloPopulation):
                 z, sfrd = self.pf['pop_sfrd']
                 
                 assert np.all(np.diff(z) > 0), "Redshifts must be ascending."
-                                
-                sfrd[sfrd <= tiny_sfrd] = tiny_sfrd
+                
+                if self.pf['pop_sfrd_units'] == 'internal':
+                    sfrd[sfrd * rhodot_cgs <= tiny_sfrd] = tiny_sfrd / rhodot_cgs
+                else:
+                    sfrd[sfrd <= tiny_sfrd] = tiny_sfrd
+                    
                 interp = interp1d(z, np.log(sfrd), kind=self.pf['pop_interp_sfrd'],
                     bounds_error=False, fill_value=-np.inf)
                     

ares/populations/GalaxyCohort.py
@@ -1263,10 +1263,21 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
         return self._tab_logMmax_    
     
     @property
+    def _loaded_guesses(self):
+        if not hasattr(self, '_loaded_guesses_'):
+            self._loaded_guesses_ = False
+        return self._loaded_guesses_
+    
+    @_loaded_guesses.setter
+    def _loaded_guesses(self, value):
+        self._loaded_guesses_ = value
+    
+    @property
     def _tab_Mmin(self):
         if not hasattr(self, '_tab_Mmin_'):
+                        
             # First, compute threshold mass vs. redshift
-            if self.pf['feedback_LW_guesses'] is not None:
+            if self.pf['feedback_LW_guesses'] is not None and (not self._loaded_guesses):
                 guess = self._guess_Mmin()
                 if guess is not None:
                     self._tab_Mmin = guess
@@ -1304,7 +1315,8 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
     def _tab_Mmin(self, value):
         if ismethod(value):
             self.Mmin = value
-            self._tab_Mmin_ = np.array(list(map(value, self.halos.tab_z)), dtype=float)
+            self._tab_Mmin_ = np.array(list(map(value, self.halos.tab_z)), 
+                dtype=float)
         elif type(value) in [int, float, np.float64]:    
             self._tab_Mmin_ = value * np.ones_like(self.halos.tab_z)
         else:
@@ -1724,6 +1736,12 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
             ##
             self._tab_sfrd_total_ = np.zeros_like(self.halos.tab_z)
             for i, z in enumerate(self.halos.tab_z):
+                
+                if z < self.pf['final_redshift']:
+                    continue
+                
+                if z > self.pf['initial_redshift']:
+                    continue
                 
                 if z > self.zform:
                     continue
@@ -3007,8 +3025,12 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
         
         if fn is None:
             return None
+            
+        if self.id_num is None:
+            return None
         
         if isinstance(fn, basestring):
+            print("Loading guesses", self._loaded_guesses, self.id_num)
             anl = ModelSet(fn)
         elif isinstance(fn, ModelSet): 
             anl = fn
@@ -3047,7 +3069,7 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
             score += np.abs(np.log10(vals) - np.log10(kw[par]))
         
         best = np.argmin(score)
-                
+                        
         return np.interp(self.halos.tab_z, zarr, Mmin[best])
         
     def save(self, prefix=None, fn=None, fmt='npz'):

ares/simulations/MetaGalacticBackground.py
@@ -192,10 +192,12 @@ class MetaGalacticBackground(AnalyzeMGB):
         count = self.count   # Just to make sure attribute exists
         self._count += 1
 
+        is_converged = self._is_Mmin_converged(self._lwb_sources)
+        
         ## 
         # Feedback
         ##
-        if self._is_Mmin_converged(self._lwb_sources):
+        if is_converged:
             self._has_fluxes = True
             self._f_Ja = lambda z: np.interp(z, self._zarr, self._Ja, 
                 left=0.0, right=0.0)
@@ -808,7 +810,7 @@ class MetaGalacticBackground(AnalyzeMGB):
         return self._LW_felt_by_    
     
     def _is_Mmin_converged(self, include_pops):
-
+        
         # Need better long-term fix: Lya sources aren't necessarily LW 
         # sources, if (for example) approx_all_pops = True. 
         if not self.pf['feedback_LW']:
@@ -833,15 +835,24 @@ class MetaGalacticBackground(AnalyzeMGB):
         # Instance of a population that "feels" the feedback.
         # Need for (1) initial _Mmin_pre value, and (2) setting ceiling
         pop_fb = self.pops[self._lwb_sources[0]]
+        
+        # Don't re-load Mmin guesses after first iteration
+        if self.pf['feedback_LW_guesses'] is not None and self.count > 1:  
+            pid = self.pf['feedback_LW_sfrd_popid']  
+            self.pops[pid]._loaded_guesses = True    
+            print('turning off ModelSet load', self.count, pid, self.pops[pid]._loaded_guesses)
+        
 
         # Save last iteration's solution for Mmin(z)
         if self.count == 1:            
             has_guess = False
             if self.pf['feedback_LW_guesses'] is not None:
+                print('hey loading guesses')
                 has_guess = True
                 pid = self.pf['feedback_LW_sfrd_popid']
                 #_z_guess, _Mmin_guess = guess
                 self._Mmin_pre = self.pops[pid].Mmin(zarr)
+                
             else:
                 self._Mmin_pre = np.min([self.pops[idnum].Mmin(zarr) \
                     for idnum in self._lwb_sources], axis=0)
@@ -859,7 +870,7 @@ class MetaGalacticBackground(AnalyzeMGB):
                         
         else:
             self._Mmin_pre = self._Mmin_now.copy()
-        
+            
         if self.pf['feedback_LW_sfrd_popid'] is not None:
             pid = self.pf['feedback_LW_sfrd_popid']
             if self.count == 1: