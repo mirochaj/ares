6e19d6b8e738bbdbe0c92d1f8d25fb85d71c0830
Jordan Mirocha <mirochaj@gmail.com>
2020-03-24 20:10:29 -0400

    apparently travis needs an __init__.py file in tests?

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ python setup.py install
 python remote.py minimal basic
 echo "backend : Agg" > $HOME/matplotlibrc
 export MATPLOTLIBRC=$HOME
-pytest tests/test*.py -v --cov-report term --cov-report html:htmlcov --cov-report xml --cov=ares 
+pytest tests/test*.py -v --cov=ares 
 
