unfortunate off-by-one parentheses placement problem now fixed in augmenting halo histories via hgh_Mmax!=None

ares/analysis/GalaxyPopulation.py
@@ -265,7 +265,7 @@ class GalaxyPopulation(object):
     def PlotColors(self, pop, axes=None, fig=1, z_uvlf=[4,6,8,10],
         z_beta=[4,5,6,7], z_only=None, sources='all', repeat_z=True, beta_phot=True, 
         show_Mstell=True, show_MUV=True, label=None, zcal=None, Mlim=-15,
-        dmag=0.5, dlam_c94=10, fill=False, extra_pane=False, square=False,
+        dmag=0.5, dlam=20, dlam_c94=10, fill=False, extra_pane=False, square=False,
         cmap=None, **kwargs):
         """
         Make a nice plot showing UVLF and UV CMD constraints and models.
@@ -470,7 +470,7 @@ class GalaxyPopulation(object):
                     else:
                         bbox = None    
                         
-                    _ax.text(0.95, 0.3-0.1*ct_lf[k], r'$z \sim {}$'.format(z),  
+                    _ax.text(0.95, 0.4-0.1*ct_lf[k], r'$z \sim {}$'.format(z),  
                         transform=_ax.transAxes, color=colors(zint), 
                         ha='right', va='top', bbox=bbox, fontsize=20)
                                             
@@ -489,7 +489,7 @@ class GalaxyPopulation(object):
                         mec=colors(zint), sources=sources, round_z=0.21, use_labels=0)
                     
                     if not had_axes:
-                        _ax2.annotate(r'$z \sim {}$'.format(zint), (0.05, 0.3-0.1*ct_lf[k]), 
+                        _ax2.annotate(r'$z \sim {}$'.format(zint), (0.05, 0.4-0.1*ct_lf[k]), 
                             xycoords='axes fraction', color=colors(zint), 
                             ha='left', va='top', fontsize=20)
 
@@ -614,9 +614,9 @@ class GalaxyPopulation(object):
                 else:
                     hst_filt = hst_shallow
             
-                cam = 'wfc', 'wfc3' if zstr <= 7 else 'nircam'    
-                filt = hst_filt[zstr] if zstr <= 7 else None
-                fset = None if zstr <= 7 else 'M'
+                cam = 'wfc', 'wfc3' if zstr <= 8 else 'nircam'    
+                filt = hst_filt[zstr] if zstr <= 8 else None
+                fset = None if zstr <= 8 else 'M'
             
                 #_mags = pop.Beta(z, Mbins=mags_cr, dlam=20.,
                 #    cam=cam, filters=filt, filter_set=fset, rest_wave=None)
@@ -624,10 +624,10 @@ class GalaxyPopulation(object):
                 if beta_phot:
                     beta = pop.Beta(z, Mbins=mags_cr, return_binned=True,
                         cam=cam, filters=filt, filter_set=fset, rest_wave=None,
-                        dlam=20.)
+                        dlam=dlam)
                 else:
                     beta = pop.Beta(z, Mbins=mags_cr, return_binned=True,
-                        rest_wave=(1600., 3000.), dlam=20.)
+                        rest_wave=(1600., 3000.), dlam=dlam)
                 
                 bphot_by_pop[h][z] = beta
                 
@@ -1279,12 +1279,12 @@ class GalaxyPopulation(object):
             if quantity in ['lf']:
                 if data[source]['wavelength'] != wavelength:
                     #shift = sed_model.
-                    print("WARNING: {0!s} wavelength={1}A, not {2}A!".format(\
+                    print("# WARNING: {0!s} wavelength={1}A, not {2}A!".format(\
                         source, data[source]['wavelength'], wavelength))
             #else:
             if source in ['stefanon2017', 'duncan2014']:
                 shift = 0.25
-                print("Shifting stellar masses by 0.25 dex (Chabrier -> Salpeter) for source={}".format(source))
+                print("# Shifting stellar masses by 0.25 dex (Chabrier -> Salpeter) for source={}".format(source))
             else:    
                 shift = 0.    
                                                     
@@ -1805,7 +1805,7 @@ class GalaxyPopulation(object):
         #ax_lae_m  = kw['ax_lae_m']
         ax_sfms   = kw['ax_sfms']
         
-        _mst  = np.arange(6, 12, 0.2)
+        _mst  = np.arange(6, 12.25, 0.25)
         _mags = np.arange(-25, -10, anl.base_kwargs['pop_mag_bin'])
 
         redshifts = [4, 6, 8, 10]

ares/populations/GalaxyCohort.py
@@ -2770,7 +2770,7 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
         zmax = []
         zform = []
         
-        if self.pf['hgh_dlogMmin'] is not None:
+        if self.pf['hgh_Mmax'] is not None:
             dMmin = self.pf['hgh_dlogMmin']
             M0_aug = np.arange(1.+dMmin, self.pf['hgh_Mmax']+dMmin, dMmin)
             results = {key:np.zeros(((zarr.size+M0_aug.size, zarr.size))) \
@@ -2809,7 +2809,7 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
         # course we'll miss out on the early histories of small halos if 
         # Tmin is large. So, fill in histories by incrementing above Mmin
         # at highest available redshsift.
-        if self.pf['hgh_dlogMmin'] is not None:
+        if self.pf['hgh_Mmax'] is not None:
             
             _z0 = zarr.max()
             i0 = zarr.size
@@ -2821,7 +2821,7 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
             
             for i, _M0 in enumerate(M0_aug):
                 # If M0 is 0, assume it's the minimum mass at this redshift.
-                _zarr, _results = self.RunSAM(z0=_z0, M0=_M0)        
+                _zarr, _results = self.RunSAM(z0=_z0, M0=_M0)
                 
                 # Need to splice into the right elements of 2-D array.
                 # SAM is run from zform to final_redshift, so only a subset
@@ -2913,7 +2913,6 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
             else:
                 print('hay problemas!', z0, self.halos.tab_z[iz])
         elif (M0 > 1):
-            _M0 = np.interp(z0, self.halos.tab_z, self._tab_Mmin)
             M0 = np.interp(z0, self.halos.tab_z, M0 * self._tab_Mmin)
 
             dM = self.pf['hgh_dlogMmin']
@@ -2923,7 +2922,7 @@ class GalaxyCohort(GalaxyAggregate,BlobFactory):
                 dM * 0.2)
             _ngtm = [self._spline_ngtm(z0, _m_) for _m_ in _marr_]
             func = interp1d(_marr_, _ngtm, kind='cubic')
-            n0 = func(np.log10(M0 - dM)) - func(np.log10(M0))
+            n0 = func(np.log10(M0)) - func(np.log10(M0) + dM)
 
         # Setup time-stepping
         zf = max(float(self.halos.tab_z.min()), self.zdead)

ares/populations/GalaxyEnsemble.py
@@ -1723,6 +1723,11 @@ class GalaxyEnsemble(HaloPopulation,BlobFactory):
 
             _x, _phi = self._cache_lf_[(z, wave)]
             
+            if self.pf['debug']:
+                print("# Read LF from cache at (z={}, wave={})".format(
+                    z, wave
+                ))
+            
             # If no x supplied, return bin centers
             if x is None:
                 return _x, _phi  
@@ -1730,8 +1735,16 @@ class GalaxyEnsemble(HaloPopulation,BlobFactory):
             if type(x) != np.ndarray:
                 k = np.argmin(np.abs(x - _x))
                 if abs(x - _x[k]) < 1e-3:
+                    if self.pf['debug']:
+                        print("# Found exact match for MUV={})".format(x))
                     return _phi[k]
                 else:
+                    if self.pf['debug']:
+                        print("# Will interpolate to MUV={}".format(x))
+                    #_func_ = interp1d(_x, np.log10(_phi), kind='cubic',
+                    #    fill_value=-np.inf)
+                    #
+                    #phi = 10**_func_(_x)
                     phi = 10**np.interp(x, _x, np.log10(_phi),
                         left=-np.inf, right=-np.inf)
                     
@@ -1742,6 +1755,14 @@ class GalaxyEnsemble(HaloPopulation,BlobFactory):
             if _x.size == x.size:
                 if np.allclose(_x, x):
                     return _phi
+                    
+            if self.pf['debug']:
+                print("# Will interpolate to new MUV array.")
+
+            #_func_ = interp1d(_x, np.log10(_phi), kind='cubic',    
+            #    fill_value=-np.inf)    
+            #
+            #return 10**_func_(x)
             
             return 10**np.interp(x, _x, np.log10(_phi), 
                 left=-np.inf, right=-np.inf)
@@ -2087,14 +2108,11 @@ class GalaxyEnsemble(HaloPopulation,BlobFactory):
 
         hist, bin_edges = np.histogram(MAB[Misok==1],
             weights=w[Misok==1], bins=bin_c2e(_x), density=True)
-            
-        bin_c = _x
-        
-        N = np.sum(w[Misok==1])
-        
+         
+        N = np.sum(w[Misok==1]) 
         phi = hist * N
-                
-        self._cache_lf_[(z, wave)] = bin_c, phi
+                          
+        self._cache_lf_[(z, wave)] = _x, phi
         
         return self._cache_lf(z, x, wave)
         
@@ -2706,7 +2724,12 @@ class GalaxyEnsemble(HaloPopulation,BlobFactory):
         
             suffix = fn[fn.rfind('.')+1:]
             path = os.getenv("ARES") + '/input/hmf/'
-            fn_hist = path + prefix.replace('hmf', 'hgh') + '.' + suffix
+            pref = prefix.replace('hmf', 'hgh') 
+            if self.pf['hgh_dlogMmin'] is not None:
+                pref += '_xM_{:.0f}_{:.2f}'.format(self.pf['hgh_Mmax'], 
+                    self.pf['hgh_dlogMmin'])
+                    
+            fn_hist = path + pref + '.' + suffix
         else:
             # Check to see if parameters match
             if self.pf['verbose']:

input/hmf/generate_halo_histories.py
@@ -20,29 +20,30 @@ import matplotlib.pyplot as pl
 try:
     fn_hmf = sys.argv[1]
 except IndexError:
-    fn_hmf = 'hmf_ST_logM_1400_4-18_z_1201_0-60.npz'
+    fn_hmf = 'hmf_ST_planck_TTTEEE_lowl_lowE_best_logM_1400_4-18_z_1201_0-60.hdf5'
 
 pars = ares.util.ParameterBundle('mirocha2017:base').pars_by_pop(0, 1) \
      + ares.util.ParameterBundle('mirocha2017:dflex').pars_by_pop(0, 1)
 
 pars['hmf_table'] = fn_hmf
 
-def_kwargs = \
-{
- "cosmology_id": 'best',
- "cosmology_name": 'planck_TTTEEE_lowl_lowE',
- #"sigma_8": 0.8159, 
- #'primordial_index': 0.9652, 
- #'omega_m_0': 0.315579, 
- #'omega_b_0': 0.0491, 
- #'hubble_0': 0.6726,
- #'omega_l_0': 1. - 0.315579,
-}
+with h5py.File(fn_hmf, 'r') as f:
+    grp = f['cosmology']
+    
+    cosmo_pars = {}
+    cosmo_pars['cosmology_name'] = grp.attrs.get('cosmology_name')
+    cosmo_pars['cosmology_id'] = grp.attrs.get('cosmology_id')
+    
+    for key in grp:
+        buff = np.zeros(1)
+        grp[key].read_direct(buff)
+        cosmo_pars[key] = buff[0]
+            
+    print("Read cosmology from {}.".format(fn_hmf))
 
-kwargs = def_kwargs.copy()
-kwargs.update(ares.util.get_cmd_line_kwargs(sys.argv[1:]))
+pars.update(cosmo_pars)
 
-pars.update(kwargs)
+# We might periodically tinker with these things but these are good defaults.
 pars['pop_Tmin'] = None
 pars['pop_Mmin'] = 1e4
 pars['hgh_dlogMmin'] = 0.1
@@ -56,6 +57,9 @@ elif 'hdf5' in fn_hmf:
     pref = fn_hmf.replace('.hdf5', '').replace('hmf', 'hgh')
 else:
     raise IOError('Unrecognized file format for HMF ({})'.format(fn_hmf))
+    
+if pars['hgh_dlogMmin'] is not None:
+    pref += '_xM_{:.0f}_{:.2f}'.format(pars['hgh_Mmax'], pars['hgh_dlogMmin'])
     
 fn = '{}.hdf5'.format(pref)
 if not os.path.exists(fn):