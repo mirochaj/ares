cfd07c560b441b4d03d2bb7525f2ce6210aa5e3e
Henri <henrilamarre@hotmail.com>
2020-04-22 15:57:11 -0400

    Sped up SynthesisModelHybrid

--- ares/sources/SynthesisModelHybrid.py
+++ ares/sources/SynthesisModelHybrid.py
@@ -18,6 +18,8 @@ from .Source import Source
 class SynthesisModelHybrid(SynthesisMaster):
 
     def __init__(self, **kwargs):
+        self.pf = kwargs
+
         Source.__init__(self, **kwargs)
         self.starburst = None
         self.bpass = None
@@ -25,15 +27,21 @@ class SynthesisModelHybrid(SynthesisMaster):
 
     @property
     def data(self):
+        if self.pf['pop_sps_data'] is not None:
+            self.bpass = self.pf['pop_sps_data'][2]
+            self.starburst = self.pf['pop_sps_data'][3]
         if self.bpass is None:
-            self.bpass = SynthesisModel(source_sed='eldridge2009', source_Z=self.pf['source_Z'], source_ssp=False)
+            model = SynthesisModel(source_sed='eldridge2009', source_Z=self.pf['source_Z'], source_ssp=False)
+            self.bpass = [model.data, model.wavelenghts, model.times]
         if self.starburst is None:
-            self.starburst = SynthesisModel(source_sed='leitherer1999',source_Z=self.pf['source_Z'], source_ssp=False)
+            model = SynthesisModel(source_sed='leitherer1999',source_Z=self.pf['source_Z'], source_ssp=False)
+            self.starburst = [model.data, model.wavelenghts, model.times]
+
         if self.interpolated_data is None:
             b_interp = 150
-            b_data = np.array(self.bpass.data).T
+            b_data = np.array(self.bpass[0]).T
             smooth_bpass = []
-            x_b = self.bpass.wavelengths
+            x_b = self.bpass[1]
             kernel_b = np.zeros_like(x_b)
             kernel_b[x_b.size//2 - b_interp//2: x_b.size//2 + b_interp//2] = np.ones(b_interp) / float(b_interp)
             smooth_bpass = np.zeros_like(b_data)
@@ -41,17 +49,17 @@ class SynthesisModelHybrid(SynthesisMaster):
                 smooth_bpass[i] = np.convolve(b_data[i], kernel_b, mode='same')
             
             s_interp = 12
-            s_data = np.array(self.starburst.data).T
+            s_data = np.array(self.starburst[0]).T
             smooth_starburst = []
-            x_s = self.starburst.wavelengths
+            x_s = self.starburst[1]
             kernel_s = np.zeros_like(x_s)
             kernel_s[x_s.size//2 - s_interp//2: x_s.size//2 + s_interp//2 ] = np.ones(s_interp) / float(s_interp)
             smooth_starburst = np.zeros_like(s_data)
             for i in range(len(smooth_starburst)):
                 smooth_starburst[i] = np.convolve(s_data[i], kernel_s, mode='same')
 
-            interpolated_b = sci.interp2d(self.bpass.wavelengths, self.bpass.times, smooth_bpass)
-            interpolated_s = sci.interp2d(self.starburst.wavelengths, self.starburst.times, smooth_starburst)
+            interpolated_b = sci.interp2d(self.bpass[1], self.bpass[2], smooth_bpass)
+            interpolated_s = sci.interp2d(self.starburst[1], self.starburst[2], smooth_starburst)
             self.interpolated_data =\
             (self.pf['source_coef']*interpolated_b(self.wavelengths, self.times)+(1-self.pf['source_coef'])*interpolated_s(self.wavelengths, self.times)).T
         
@@ -59,22 +67,34 @@ class SynthesisModelHybrid(SynthesisMaster):
 
     @property
     def wavelengths(self):
+        if self.pf['pop_sps_data'] is not None:
+            self.bpass = self.pf['pop_sps_data'][2]
+            self.starburst = self.pf['pop_sps_data'][3]
         if self.bpass is None:
-            self.bpass = SynthesisModel(source_sed='eldridge2009', source_Z=self.pf['source_Z'], source_ssp=False)
+            model = SynthesisModel(source_sed='eldridge2009', source_Z=self.pf['source_Z'], source_ssp=False)
+            self.bpass = [model.data, model.wavelenghts, model.times]
         if self.starburst is None:
-            self.starburst = SynthesisModel(source_sed='leitherer1999',source_Z=self.pf['source_Z'], source_ssp=False)
-        w_space = np.logspace(np.log10(max(self.bpass.wavelengths[0], self.starburst.wavelengths[0])),
-                                       np.log10(min(self.bpass.wavelengths[-1], self.starburst.wavelengths[-1])),
-                                       min(len(self.bpass.wavelengths), len(self.starburst.wavelengths)))
+            model = SynthesisModel(source_sed='leitherer1999',source_Z=self.pf['source_Z'], source_ssp=False)
+            self.starburst = [model.data, model.wavelenghts, model.times]
+
+        w_space = np.logspace(np.log10(max(self.bpass[1][0], self.starburst[1][0])),
+                                       np.log10(min(self.bpass[1][-1], self.starburst[1][-1])),
+                                       min(len(self.bpass[1]), len(self.starburst[1])))
         return w_space
 
     @property
     def times(self):
+        if self.pf['pop_sps_data'] is not None:
+            self.bpass = self.pf['pop_sps_data'][2]
+            self.starburst = self.pf['pop_sps_data'][3]
         if self.bpass is None:
-            self.bpass = SynthesisModel(source_sed='eldridge2009', source_Z=self.pf['source_Z'], source_ssp=False)
+            model = SynthesisModel(source_sed='eldridge2009', source_Z=self.pf['source_Z'], source_ssp=False)
+            self.bpass = [model.data, model.wavelenghts, model.times]
         if self.starburst is None:
-            self.starburst = SynthesisModel(source_sed='leitherer1999',source_Z=self.pf['source_Z'], source_ssp=False)
-        t_space = np.linspace(max(self.bpass.times[0], self.starburst.times[0]),
-                    min(self.bpass.times[-1], self.starburst.times[-1]),
-                    min(len(self.bpass.times), len(self.starburst.times)))
+            model = SynthesisModel(source_sed='leitherer1999',source_Z=self.pf['source_Z'], source_ssp=False)
+            self.starburst = [model.data, model.wavelenghts, model.times]
+
+        t_space = np.linspace(max(self.bpass[2][0], self.starburst[2][0]),
+                    min(self.bpass[2][-1], self.starburst[2][-1]),
+                    min(len(self.bpass[2]), len(self.starburst[2])))
         return t_space
