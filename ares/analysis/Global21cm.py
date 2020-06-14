some units fixes in bh modeling and likely unnecessary edits in analysis routines

ares/analysis/Global21cm.py
@@ -501,9 +501,8 @@ class Global21cm(MultiPhaseMedium,BlobFactory):
     
         if yscale == 'linear':
             if (not gotax) or force_draw:
-                ax.set_yticks(np.arange(int(ymin / 50) * 50, 
-                    100, 50) * conv)
-                
+                yticks = np.arange(int(ymin / 50) * 50, 100, 50) * conv
+                ax.set_yticks(yticks)
             else:
                 # Minor y-ticks - 10 mK increments
                 yticks = np.linspace(ymin, 50, int((50 - ymin) / 10. + 1)) * conv
@@ -527,19 +526,24 @@ class Global21cm(MultiPhaseMedium,BlobFactory):
             ax.set_xticks(xticks, minor=False)
             ax.set_xticks(xticks_minor, minor=True)
 
-            if rotate_xticks:
-                xt = []
-                for x in ax.get_xticklabels():
-                    xt.append(x.get_text())
 
-                ax.set_xticklabels(xt, rotation=45.)
-            if rotate_yticks:
-                yt = []
-                for y in ax.get_yticklabels():
-                    yt.append(y.get_text())
+            xt = []
+            for x in ax.get_xticklabels():
+                xt.append(x.get_text())
+
+            ax.set_xticklabels(xt, rotation=45. if rotate_xticks else 0)
             
-                ax.set_yticklabels(yt, rotation=45.)    
-                    
+            yt = []
+            for y in ax.get_yticklabels():
+                if not y.get_text().strip():
+                    break
+                yt.append(y.get_text())
+            
+            if yt == []:
+                yt = yticks
+            
+            ax.set_yticklabels(yt, rotation=45. if rotate_yticks else 0)    
+                                     
         if ax.get_xlabel() == '':  
             if xaxis == 'z':  
                 ax.set_xlabel(labels['z'], fontsize='x-large')

ares/analysis/ModelSet.py
@@ -3941,7 +3941,7 @@ class ModelSet(BlobFactory):
                     loc = psorted[int(N / 2.)]
                 else:
                     loc = np.argmax(self.logL[keep == 1])
-                                
+                                                                
             # A few NaNs ruin everything
             if np.any(nans):
                 print("WARNING: {} elements with NaNs detected in field={}. Will be discarded.".format(nans.sum(), name))