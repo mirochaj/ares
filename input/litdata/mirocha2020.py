e1da6b181d7e9766f8bd2bd85eeb94ec6bc05fa6
Jordan Mirocha <mirochaj@gmail.com>
2020-07-27 16:42:52 -0400

    need mreg_eduty_edtmr in litdata

--- input/litdata/mirocha2020.py
+++ input/litdata/mirocha2020.py
@@ -25,7 +25,7 @@ _base = \
  'pq_func_par8[0]': 0.0,   # Only use if slopes evolve, e.g., in dplp_evolNPS
  'pq_func_par9[0]': 0.0,   # Only use if slopes evolve, e.g., in dplp_evolNPS
  'pq_val_ceil[0]': 1.,     # Cap SFE at <= 1
-} 
+}
 
 _sed_updates = \
 {
@@ -378,7 +378,24 @@ _dtmr_best = \
  "pq_func_par0[33]": 0.181775151977,
 }
 
-_duty_dtmr_best = \
+_duty_dtmr_best_ereg = \
+{
+'pq_func_par0[0]': 0.1675,
+'pq_func_par1[0]': 59962271529.3613,
+'pq_func_par3[0]': -0.3825,
+'pq_func_par0[40]': 0.5841,
+'pq_func_par2[40]': 0.1258,
+'pq_func_par4[40]': -1.8654,
+'pq_func_par0[22]': 0.8626,
+'pq_func_par2[22]': 0.9404,
+'pq_func_par1[22]': 92768180539.0610,
+'pq_func_par0[50]': 0.4333,
+'pq_func_par2[50]': 0.2162,
+'pq_func_par4[50]': -1.3150,
+'pq_func_par0[33]': 0.0154,
+}
+
+_duty_dtmr_best_mreg = \ # NEEDS UPDATING
 {
 'pq_func_par0[0]': 0.1675,
 'pq_func_par1[0]': 59962271529.3613,
@@ -409,12 +426,16 @@ ereg_ekappa_edtmr.update(_kappa_evol)
 
 ereg_eduty_edtmr = ereg_edtmr.copy()
 ereg_eduty_edtmr.update(_fduty)
-ereg_eduty_edtmr.update(_duty_dtmr_best)
+ereg_eduty_edtmr.update(_duty_dtmr_best_ereg)
 
 evol = ereg_eduty_edtmr
 
 mreg_edtmr = ereg_edtmr.copy()
 mreg_edtmr.update(_evol_mreg_dplrd)
+
+mreg_eduty_edtmr = mreg_edtmr.copy()
+mreg_eduty_edtmr.update(_fduty)
+mreg_eduty_edtmr.update(_duty_dtmr_best_mreg)
 
 
 univ_plRd = univ.copy()
