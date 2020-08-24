3ce37babbd9c96bd5ad4259b48014001fb377d39
FuturePhelps <felbilodeau@gmail.com>
2020-08-24 11:05:43 -0400

    added experimental mode, added T_dust parametrization and cleaner calculation

--- ares/sources/DustPopulation.py
+++ ares/sources/DustPopulation.py
@@ -7,13 +7,17 @@ Affiliation: McGill University
 Created on: Thursday 9 July 2020 17:03:55 EDT
 
 Description: Contains a class which calculates the dust emissions
-for each galaxy in a GalaxyEnsemble object.
+for each galaxy in a GalaxyEnsemble object based on Imara et al. (2018).
 
 """
 
 import numpy as np
 from scipy.integrate import simps
 from ares.physics.Constants import c, h, k_B, g_per_msun, cm_per_kpc, Lsun
+
+# T_dust parameters
+PAR_ALPHA = -60
+PAR_K = 1e13
 
 class DustPopulation:
     def __init__(self, galaxyEnsemble, fmin = 1e14, fmax = 1e17, Nfreqs = 500,\
@@ -179,18 +183,102 @@ class DustPopulation:
         """
         (void) -> 2darray
 
-        Fetches and / or returns the dust mass in each galaxy at each redshift in solar masses.
+        Calculates and / or returns the dust mass in each galaxy at each redshift in solar masses.
 
         first axis: galaxy index
         second axis: redshift index
         """
         if not hasattr(self, '_M_dust'):
-            self._M_dust = np.zeros((self.Ngalaxies, self.Nz))
+            if (self.pf.get('pop_dust_experimental') is None) or (not self.pf['pop_dust_experimental']):
+                self._M_dust = np.zeros((self.Ngalaxies, self.Nz))
 
-            for i in range(self.Nz):
-                self._M_dust[:,i] = self.pop.get_field(self.z[i], 'Md')
+                for i in range(self.Nz):
+                    self._M_dust[:,i] = self.pop.get_field(self.z[i], 'Md')
+
+            elif self.pf['pop_dust_experimental']:
+                self._M_dust = self.M_gas * self.DGR
+                NaNs = np.isnan(self._M_dust)
+                self._M_dust[NaNs] = 0
+            
+            else:
+                raise ValueError("Parameter 'pop_dust_experimental' must be True, False, or non-existent (None)")
 
         return self._M_dust
+
+    @property
+    def Z(self):
+        """
+        (void) -> 2darray
+
+        Calculates and / or returns the metallicity calculated from the Imara et al. (2018) paper.
+        The convention is Z = 12 + log(O/H). This is only used in experimental mode
+        and is NOT self-consistent.
+
+        first axis: galaxy index
+        second axis: redshift index
+        """
+        if not hasattr(self, '_Z'):
+            SFR = np.zeros((self.Ngalaxies, self.Nz))
+            M_star = np.zeros((self.Ngalaxies, self.Nz))
+
+            for i in range(self.Nz):
+                SFR[:,i] = self.pop.get_field(self.z[i], 'SFR')
+                M_star[:,i] = self.pop.get_field(self.z[i], 'Ms')
+
+            print("SFR =", SFR)
+            fq = 1 / (M_star / (10**(10.2 + 0.5 * self.z[None,:])) + 1)
+            SFR /= 1 - fq
+
+            self._Z = -0.14*np.log10(SFR) + 0.37 * np.log10(M_star) + 4.82
+            NaNs = np.isnan(self._Z)
+            self._Z[NaNs] = 0
+
+        return self._Z
+
+    @property
+    def DGR(self):
+        """
+        (void) -> 2darray
+
+        Calculates and / or returns the Dust-to-Gas Ratio (DGR) for all galaxies at all redshifts.
+        For now, this is only used in experimental mode and is NOT self-consistent. Based on
+        Imara et al. (2018).
+
+        first axis: galaxy index
+        second axis: redshift index
+        """
+        if not hasattr(self, '_DGR'):
+            Z_SUN = 8.7 # based on Asplund et al. (2009)
+            log_of_ratio = np.log10(self.Z / Z_SUN)
+            small_Z = self.Z <= 0.26 * Z_SUN
+            log_of_ratio[small_Z] = 3.15 * log_of_ratio[small_Z] + 1.25
+            log_DGR = log_of_ratio - 2.21
+            self._DGR = 10**log_DGR
+            NaNs = np.isnan(self._DGR)
+            self._DGR[NaNs] = 0
+
+        return self._DGR
+
+    @property
+    def M_gas(self):
+        """
+        (void) -> 2darray
+
+        Calculates and / or returns the gas mass based on Imara et al. This is only used in
+        experimental mode, and is NOT self-consistent. Based on Imara et al. (2018)
+
+        first axis: galaxy index
+        second axis: redshift index
+        """
+        if not hasattr(self, '_M_gas'):
+            M_star = np.zeros((self.Ngalaxies, self.Nz))
+            for i in range(self.Nz):
+                M_star[:,i] = self.pop.get_field(self.z[i], 'Ms')
+
+            self._M_gas = 3.87e9 * (1 + self.z[None,:])**1.35 * (M_star / 1e10)**0.49
+
+        return self._M_gas
+
 
     @property
     def kappa_nu(self):
@@ -245,6 +333,89 @@ class DustPopulation:
         
         return self._T_cmb
 
+    def __T_dust(self, z, L_nu, tau_nu, R_dust, T_cmb):
+        """
+        (1darray, 3darray, 3darray, 2darray, 2darray) -> 2darray
+
+        Calculates and returns the dust temperature for each galaxy at each redshift.
+        If 'pop_dust_experimental' is False, this is based on Imara et al (2018).
+        If 'pop_dust_experimental' is True, this is a log-linear parametrization.
+
+        first axis: galaxy index
+        second axis: redshift index
+        """
+        # --------------------------------------------------------------------------------------------------
+        Ngalaxies = L_nu.shape[0]
+        Nz = len(z)
+
+        # Calculate total power absorbed per dust mass
+        if (self.pf.get('pop_dust_experimental') is None) or (not self.pf['pop_dust_experimental']):
+
+            if (self.pf.get('pop_dust_distrib') is None) or (self.pf['pop_dust_distrib'] == 'homogeneous'):
+                f_geom = (1 - np.exp(-tau_nu)) / tau_nu
+
+            elif self.pf['pop_dust_distrib'] == 'pt src':
+                f_geom = np.exp(-tau_nu)
+
+            else:
+                raise ValueError("Parameter pop_dust_distrib must be 'homogeneous' or 'pt src'.")
+
+            kappa_nu = np.zeros((Ngalaxies, self.Nfreqs, Nz))
+            kappa_nu += (0.1 * (self.frequencies / 1e12)**2)[None, :, None]
+
+            tmp_stellar = L_nu * f_geom \
+                * self.pop.histories['fcov'] * kappa_nu \
+                / (R_dust[:,None,:] * cm_per_kpc)**2
+
+            
+            cmb_freqs = np.linspace(1, 1e14, 1000)
+
+            cmb_kappa_nu = np.zeros((Ngalaxies, 1000, Nz))
+            cmb_kappa_nu += (0.1 * (cmb_freqs / 1e12)**2)[None, :, None]
+            
+            tmp_cmb = 8 * np.pi * h / c**2 * cmb_kappa_nu * (cmb_freqs[None, :, None])**3 \
+                / (np.exp(h * cmb_freqs[None,:,None] / k_B / T_cmb[:,None,:]) - 1)
+
+            tmp_power = simps(tmp_stellar, self.frequencies, axis = 1)
+            tmp_power += simps(tmp_cmb, cmb_freqs, axis = 1)
+
+            if self.pf.get('pop_dust_experimental'):
+                print("power =", tmp_power)
+
+            # This prefactor is based on analytically performing the integral,
+            # so getting the dust temperature for a different model
+            # would work differently
+
+            tmp_prefactor = 64e-25 / 63 * np.pi**7 * k_B**6 / c**2 / h**5
+            T_dust = (tmp_power / tmp_prefactor)**(1/6)
+            NaNs = np.isnan(T_dust)
+            T_dust[NaNs] = 0.
+        
+        elif self.pf['pop_dust_experimental']:
+            # Log-linear parametrization
+            # For now, this is redshift independent,
+            # probably need to add redshift dependence to it at some point
+            M_star = np.zeros((Ngalaxies, Nz))
+            for i in range(Nz):
+                M_star[:,i] = self.pop.get_field(z[i], 'Ms')
+            
+            # Parameters
+
+            PAR_ALPHA = -60
+            PAR_K = 1e13
+            
+            # Calculation
+
+            T_dust = PAR_ALPHA * np.log10(M_star /  PAR_K)
+            T_dust[M_star == 0] = 0
+        
+        else:
+            raise ValueError("Parameter 'pop_dust_experimental' must be True, False, or non-existent (None)")
+
+        return T_dust
+
+        # --------------------------------------------------------------------------------------------------
+
     @property
     def T_dust(self):
         """
@@ -261,42 +432,8 @@ class DustPopulation:
         has no galaxy at all)
         """
         if not hasattr(self, '_T_dust'):
-            # Calculate total power absorbed per dust mass
 
-            if (self.pf.get('pop_dust_distrib') is None) or (self.pf['pop_dust_distrib'] == 'homogeneous'):
-                f_geom = (1 - np.exp(-self.tau_nu)) / self.tau_nu
-
-            elif self.pf['pop_dust_distrib'] == 'pt src':
-                f_geom = np.exp(-self.tau_nu)
-
-            else:
-                raise ValueError("Parameter pop_dust_distrib must be 'homogeneous' or 'pt src'.")
-
-            tmp_stellar = self.L_nu * f_geom \
-                * self.pop.histories['fcov'] * self.kappa_nu \
-                / (self.R_dust[:,None,:] * cm_per_kpc)**2
-
-            
-            cmb_freqs = np.linspace(1, 1e14, 1000)
-
-            cmb_kappa_nu = np.zeros((self.Ngalaxies, 1000, self.Nz))
-            cmb_kappa_nu += (0.1 * (cmb_freqs / 1e12)**2)[None, :, None]
-            
-            tmp_cmb = 8 * np.pi * h / c**2 * cmb_kappa_nu * (cmb_freqs[None, :, None])**3 \
-                / (np.exp(h * cmb_freqs[None,:,None] / k_B / self.T_cmb[:,None,:]) - 1)
-
-            tmp_power = simps(tmp_stellar, self.frequencies, axis = 1)
-            tmp_power += simps(tmp_cmb, cmb_freqs, axis = 1)
-
-            # This prefactor is based on analytically performing the integral,
-            # so getting the dust temperature for a different model
-            # would work differently
-
-            tmp_prefactor = 64e-25 / 63 * np.pi**7 * k_B**6 / c**2 / h**5
-            tmp_T_dust = (tmp_power / tmp_prefactor)**(1/6)
-            NaNs = np.isnan(tmp_T_dust)
-            tmp_T_dust[NaNs] = 0.
-            self._T_dust = tmp_T_dust
+            self._T_dust = self.__T_dust(self.z, self.L_nu, self.tau_nu, self.R_dust, self.T_cmb)
 
         return self._T_dust
 
@@ -386,48 +523,25 @@ class DustPopulation:
 
         # is not cached, we calculate everything for the given z and wave
         else:
-            # All this code is just to get the dust temperature
-            # (if we want to not repeat code maybe need to rewrite the class
-            # in a more modular way)
+            # This code calculates the dust temperature
+            M_dust = self.pop.get_field(z, 'Md')
 
             waves = c / self.frequencies * 1e8
+
             L_nu = self.pop.synth.Spectrum(waves, \
-                    zobs = z, sfh = self.histories['SFR'], tarr = self.histories['t'])                      # 2darray galaxy, frequency (in ergs / s / Hz)
-            
-            R_dust = self.pop.halos.VirialRadius(z, self.pop.get_field(z, 'Mh')) * 0.018                    # 1darray galaxy (in kpc)
+                        zobs = z, sfh = self.histories['SFR'], tarr = self.histories['t'])
 
-            kappa_nu = 0.1 * (self.frequencies / 1e12)**2                                                   # 1darray frequency (in cm^2/g)
+            R_dust = self.pop.halos.VirialRadius(z, self.pop.get_field(z, 'Mh')) * 0.018
 
-            M_dust = self.pop.get_field(z, 'Md')                                                            # 1darray galaxy (in solar masses)
+            kappa_nu = 0.1 * (self.frequencies / 1e12)**2
 
             tau_nu = 3 * (M_dust[:, None] * g_per_msun) * kappa_nu[None, :]\
-                 / (4 * np.pi * (R_dust[:, None] * cm_per_kpc)**2)                                          # 2darray galaxy, frequency (dim. less)
+                    / (4 * np.pi * (R_dust[:, None] * cm_per_kpc)**2)
 
-            f_geom = (1 - np.exp(-tau_nu)) / tau_nu                                                         # 2darray galaxy, frequency (dim. less)
+            T_cmb = self.pop.cosm.TCMB(z)
 
-            f_star = self.pop.histories['fcov']                                                             # just a number (dim. less)
-
-            T_cmb = self.pop.cosm.TCMB(z)                                                                   # just a number (in K)
-
-            stellar = L_nu * f_geom * f_star * kappa_nu[None, :] \
-                / (R_dust[:, None] * cm_per_kpc)**2                                                         # 2darray galaxy, frequency
-
-            
-            cmb_freqs = np.linspace(1, 1e14, 1000)
-
-            cmb_kappa_nu = 0.1 * (cmb_freqs / 1e12)**2
-            
-            cmb = 8 * np.pi * h / c**2 * cmb_kappa_nu * (cmb_freqs)**3 \
-                / (np.exp(h * cmb_freqs / k_B / T_cmb) - 1)                                                 # 1darray frequency
-
-            power = simps(stellar, self.frequencies, axis = 1)                                              # 1darray galaxy
-            power += simps(cmb, cmb_freqs)
-
-            prefactor = 64e-25 / 63 * np.pi**7 * k_B**6 / c**2 / h**5                                       # just a number, cgs units
-
-            T_dust = (power / prefactor)**(1/6)
-            NaNs = np.isnan(T_dust)
-            T_dust[NaNs] = 0                                                                                # 1darray galaxy
+            T_dust = self.__T_dust(np.array([z]), L_nu[:,:,None], tau_nu[:,:,None], R_dust[:,None], np.array([T_cmb])[:,None])
+            T_dust = T_dust[:,0]
 
             # Now we can finally calculate the luminosities
             
@@ -435,14 +549,14 @@ class DustPopulation:
                 nu = c / wave * 1e8
                 kappa_nu = 0.1 * (nu / 1e12)**2
                 luminosities = 8 * np.pi * h / c**2 * nu**3 * kappa_nu \
-                    / (np.exp(h * nu / k_B / T_dust) - 1) * (M_dust * g_per_msun)                           # 1darray galaxy (ergs / s / Hz)
+                    / (np.exp(h * nu / k_B / T_dust) - 1) * (M_dust * g_per_msun)
             else:
-                fmax = c / (8 * 1e-4)
-                fmin = c / (1000 * 1e-4)
-                nu = np.linspace(fmin, fmax, 1000)                                                          # 1darray freqs (Hz)
-                kappa_nu = 0.1 * (nu / 1e12)**2                                                             # 1darray freqs (cm^2 / g)
+                fmax = c / (8e-4)
+                fmin = c / (1000e-4)
+                nu = np.linspace(fmin, fmax, 1000)
+                kappa_nu = 0.1 * (nu / 1e12)**2
                 luminosities = 8 * np.pi * h / c**2 * nu[None,:]**3 * kappa_nu[None,:] \
-                    / (np.exp(h * nu[None,:] / k_B / T_dust[:,None]) - 1) * (M_dust[:,None] * g_per_msun)   # 2darray galaxy, freqs (ergs / s / Hz)
+                    / (np.exp(h * nu[None,:] / k_B / T_dust[:,None]) - 1) * (M_dust[:,None] * g_per_msun)
                 luminosities = simps(luminosities, nu, axis = 1)
 
 
