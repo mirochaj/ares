fix CCL units, and add density unit unit tests

ares/physics/Cosmology.py
@@ -543,53 +543,53 @@ class CosmologyARES(InitialConditions):
             return self.hubble_0 * np.sqrt(self.omega_m_0) * (1. + z)**1.5
         return self.hubble_0 * np.sqrt(self.EvolutionFunction(z))
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def HubbleLength(self, z):
         return c / self.HubbleParameter(z)
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def HubbleTime(self, z):
         return 1. / self.HubbleParameter(z)
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def OmegaMatter(self, z):
         if self.approx_highz:
             return 1.0
         return self.omega_m_0 * (1. + z)**3 / self.EvolutionFunction(z)
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def OmegaLambda(self, z):
         if self.approx_highz:
             return 0.0
 
         return self.omega_l_0 / self.EvolutionFunction(z)
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def MeanMatterDensity(self, z):
         return self.OmegaMatter(z) * self.CriticalDensity(z)
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def MeanDarkMatterDensity(self, z):
         return (self.omega_cdm_0 / self.omega_m_0) * self.MeanMatterDensity(z)
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def MeanBaryonDensity(self, z):
         return (self.omega_b_0 / self.omega_m_0) * self.MeanMatterDensity(z)
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def MeanHydrogenNumberDensity(self, z):
         return (1. - self.Y) * self.MeanBaryonDensity(z) / m_H
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def MeanHeliumNumberDensity(self, z):
         return self.Y * self.MeanBaryonDensity(z) / m_He
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def MeanBaryonNumberDensity(self, z):
         return self.MeanBaryonDensity(z) / (m_H * self.MeanHydrogenNumberDensity(z) +
             4. * m_H * self.y * self.MeanHeliumNumberDensity(z))
 
-    @lru_cache(maxsize = 100)
+    # @lru_cache(maxsize = 100)
     def CriticalDensity(self, z):
         return (3.0 * self.HubbleParameter(z)**2) / (8.0 * np.pi * G)
 

tests/test_physics_cosmology.py
@@ -10,45 +10,79 @@ Description:
 
 """
 
+import pytest
 import numpy as np
 from ares.physics import Cosmology
 from ares.physics.Constants import s_per_gyr, m_H, m_He, cm_per_mpc
 
-def test(rtol=1e-3):
-    
-    cosm = Cosmology()
-    
+
+@pytest.mark.parametrize("cosmology_package", [None, "ccl"])
+def test(cosmology_package, rtol=2e-3):
+
+    cosm = Cosmology(cosmology_package=cosmology_package)
+
     # Check some high-z limits
     cosm_appr = Cosmology(approx_highz=True)
-    
+
     # Check critical density
-    assert cosm.CriticalDensity(0.) == cosm.CriticalDensityNow
-    
+    assert cosm.CriticalDensity(0.0) == cosm.CriticalDensityNow
+
     # Make sure energy densities sum to unity
-    assert np.allclose(cosm.omega_m_0, 1. - cosm.omega_l_0)
-    
+    assert np.allclose(cosm.omega_m_0, 1.0 - cosm.omega_l_0)
+
     # Make sure the age of the Universe is OK
-    assert 13.5 <= cosm.t_of_z(0.) / s_per_gyr <= 14.
-    
+    assert 13.5 <= cosm.t_of_z(0.0) / s_per_gyr <= 14.0
+
     # Check high-z limit for Hubble parameter. Better than 1%?
-    H_n = cosm.HubbleParameter(30.)
-    H_a = cosm_appr.HubbleParameter(30.)    
-    assert abs(H_n - H_a) / H_a < rtol, \
-        "Hubble parameter @ high-z not accurate to < {:.3g}%.".format(rtol)
-        
+    H_n = cosm.HubbleParameter(30.0)
+    H_a = cosm_appr.HubbleParameter(30.0)
+    assert abs(H_n - H_a) / H_a < rtol, "Hubble parameter @ high-z not accurate to < {:.3g}%.".format(rtol)
+
     # Check high-z limit for comoving radial distance
-    R_n = cosm_appr.ComovingRadialDistance(20., 30.) / cm_per_mpc
-    R_a = cosm.ComovingRadialDistance(20., 30.) / cm_per_mpc
-    
-    assert abs(R_a - R_n) / R_a < rtol, \
-        "Comoving radial distance @ high-z not accurate to < {:.3g}%.".format(rtol)
-        
+    R_n = cosm_appr.ComovingRadialDistance(20.0, 30.0) / cm_per_mpc
+    R_a = cosm.ComovingRadialDistance(20.0, 30.0) / cm_per_mpc
+
+    assert (
+        abs(R_a - R_n) / R_a < rtol
+    ), "Comoving radial distance @ high-z not accurate to < {:.3g}%.".format(rtol)
+
+    zs = np.linspace(0, 200, 100)
+    mean_matter_density = cosm.MeanMatterDensity(zs)
+    print(mean_matter_density)
+    assert (mean_matter_density > 0).all()
+
+    dtdz = cosm.dtdz(zs)
+    assert (dtdz > 0).all()
+
     # Test a user-supplied cosmology and one that grabs a row from Planck chain
     # Remember: test suite doesn't have CosmoRec, so don't use get_inits_rec.
-    cosm = Cosmology(cosmology_name='user', cosmology_id='jordan')
-    
-    cosm = Cosmology(cosmology_id=100)
-                
-if __name__ == '__main__':
-    test()    
+    cosm = Cosmology(cosmology_name="user", cosmology_id="jordan")
 
+    cosm = Cosmology(cosmology_id=100)
+
+
+@pytest.mark.parametrize(
+    "attr",
+    [
+        "CriticalDensity",
+        "MeanMatterDensity",
+        "MeanDarkMatterDensity",
+        "MeanBaryonDensity",
+        "MeanHydrogenNumberDensity",
+        "MeanHeliumNumberDensity",
+        "MeanBaryonNumberDensity",
+    ],
+)
+def test_density_units(attr, rtol=0.1):
+    cosm = Cosmology()
+    cosm_ccl = Cosmology(cosmology_package="ccl")
+
+    zs = np.linspace(0, 200, 100)
+    density = getattr(cosm, attr)
+    density_ccl = getattr(cosm_ccl, attr)
+    rel_diff = np.abs(density_ccl(zs) - density(zs)) / density(zs)
+    assert (rel_diff < rtol).all()
+
+
+if __name__ == "__main__":
+    test()

tests/test_simulations_gs_4par.py
@@ -10,87 +10,97 @@ Description:
 
 """
 
+import pytest
+
 import ares
 import numpy as np
 import matplotlib.pyplot as pl
 
-def test():
-    sim = ares.simulations.Global21cm()
-    
+
+@pytest.mark.parametrize("cosmology_package", [None, "ccl"])
+@pytest.mark.parametrize("initial_redshift", [40, 60, 80])
+@pytest.mark.parametrize("radiative_transfer", [False, True])
+# @pytest.mark.parametrize('check_results', [False, True])
+def test(cosmology_package, initial_redshift, radiative_transfer, check_results=True):
+    sim = ares.simulations.Global21cm(
+        cosmology_package=cosmology_package,
+        initial_redshift=initial_redshift,
+        radiative_transfer=radiative_transfer,
+    )
+
     sim.info
     pf = sim.pf
     sim.pf._check_for_conflicts()
     assert sim.pf.Npops == 3
-    
+
     sim.run()
     ax, zax = sim.GlobalSignature(fig=0, ymin=-400)
-    
-    sim.AdiabaticFloor(ax, color='k', ls=':')
+
+    sim.AdiabaticFloor(ax, color="k", ls=":")
     sim.AdiabaticFloor(ax)
-    
+
     sim.SaturatedLimit(ax)
-    
+
     inset_tau = sim.add_tau_inset(ax)
     inset_Ts = sim.add_Ts_inset(ax)
-    
+
     ax1b, zax1b = sim.GlobalSignature(fig=1, ymin=-400, time_ax=True)
-    
+
+    if not radiative_transfer:
+        return
     #
     # Make sure it's not a null signal.
-    z = sim.history['z']
-    dTb = sim.history['dTb'][z < 50]
+    z = sim.history["z"]
+    dTb = sim.history["dTb"][z < 50]
     assert len(np.unique(np.sign(dTb))) == 2
     assert max(dTb) > 5 and min(dTb) < -5
-    
+
     # Test that the turning points are there, that tau_e is reasonable, etc.
     assert 80 <= sim.z_A <= 90
     assert 10 <= sim.nu_A <= 20
     assert -50 <= sim.dTb_A <= -40
-    
+
     assert 25 <= sim.z_B <= 35
     assert -15 <= sim.dTb_B <= 0
-    
+
     assert 10 <= sim.z_C <= 25
     assert -250 <= sim.dTb_C <= 0
-    
+
     assert 6 <= sim.z_D <= 15
     assert 0 <= sim.dTb_D <= 30
-    
+
     assert 0.04 <= sim.tau_e <= 0.15
-    
+
     fwhm = sim.Width()
     hwhm = sim.Width(peak_relative=True)
-    
+
     assert 10 <= fwhm <= 50
     assert 0 <= hwhm <= 3
-    
+
     k = sim.kurtosis
     s = sim.skewness
-    
+
     slope1 = sim.dTbdz
     slope2 = sim.dTbdnu
     curv1 = sim.dTb2dz2
     curv2 = sim.dTb2dnu2
-        
-    ax2 = sim.OpticalDepthHistory(fig=2, show_obs=True, 
-        obs_mu=0.055, obs_sigma=0.009)
+
+    ax2 = sim.OpticalDepthHistory(fig=2, show_obs=True, obs_mu=0.055, obs_sigma=0.009)
     ax3 = sim.TemperatureHistory(fig=3)
-    ax3 = sim.TemperatureHistory(ax=ax3, show_Ts=True, show_Tk=False, 
-        show_Tcmb=True)
+    ax3 = sim.TemperatureHistory(ax=ax3, show_Ts=True, show_Tk=False, show_Tcmb=True)
     ax4 = sim.IonizationHistory(fig=4)
     ax5 = sim.GlobalSignatureDerivative(fig=5)
     ax6 = sim.GlobalSignatureDerivative(fig=6, show_signal=True)
-    
-    sim.save('test_gs_4par', 'pkl', clobber=True)
-    sim.save('test_gs_4par', 'txt', clobber=True)
-    
+
+    sim.save("test_gs_4par", "pkl", clobber=True)
+    sim.save("test_gs_4par", "txt", clobber=True)
+
     for i in range(0, 6):
         pl.figure(i)
-        pl.savefig('{0!s}_{1}.png'.format(__file__[0:__file__.rfind('.')], i))     
-    
-    #pl.close('all')
-    
-if __name__ == '__main__':
+        pl.savefig("{0!s}_{1}.png".format(__file__[0 : __file__.rfind(".")], i))
+
+    # pl.close('all')
+
+
+if __name__ == "__main__":
     test()
-
-