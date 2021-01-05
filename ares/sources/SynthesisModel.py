some bugfixes to enable SynthesisModelToy use in NIRB. Also fixed machine precision issues when tau_clumpy=madau1995

ares/sources/SynthesisModel.py
@@ -33,8 +33,9 @@ class SynthesisModelBase(Source):
     def _neb_cont(self):
         if not hasattr(self, '_neb_cont_'):
             self._neb_cont_ = np.zeros_like(self._data)
-            if self.pf['source_nebular_continuum']:
-                assert self.pf['source_nebular'] > 1
+            if self.pf['source_nebular'] > 1 and \
+                self.pf['source_nebular_continuum']:
+
                 for i, t in enumerate(self.times):
                     if self.pf['source_tneb'] is not None:
                         j = np.argmin(np.abs(self.pf['source_tneb'] - self.times))
@@ -55,8 +56,8 @@ class SynthesisModelBase(Source):
     def _neb_line(self):
         if not hasattr(self, '_neb_line_'):
             self._neb_line_ = np.zeros_like(self._data)
-            if self.pf['source_nebular_lines']:
-                assert self.pf['source_nebular'] > 1
+            if self.pf['source_nebular'] > 1 and \
+                self.pf['source_nebular_lines']:
                 for i, t in enumerate(self.times):
                     if self.pf['source_tneb'] is not None:
                         j = np.argmin(np.abs(self.pf['source_tneb'] - self.times))

ares/sources/SynthesisModelToy.py
@@ -33,13 +33,13 @@ class SynthesisModelToy(SynthesisModelBase):
                     / erg_per_ev
             else:
                 dE = self.pf['source_dE']
-                dl = self.pf['source_dlam']
-                if dE is not None:
-                    self._energies = np.arange(self.Emin, self.Emax+dE, dE)
-                else:
-                    self._energies = h_p * c / self.wavelengths / cm_per_ang
+                Emin = self.pf['source_Emin']
+                Emax = self.pf['source_Emax']
 
-                assert (dE is not None) + (dl is not None) == 1
+                self._energies = np.arange(Emin, Emax+dE, dE)[-1::-1]
+
+            # Should be in descending order
+            assert np.all(np.diff(self._energies) < 0)
 
         return self._energies
 
@@ -53,18 +53,14 @@ class SynthesisModelToy(SynthesisModelBase):
                     self.pf['source_lmax']+self.pf['source_dlam'],
                     self.pf['source_dlam'])
             else:
-                dE = self.pf['source_dE']
-                dl = self.pf['source_dlam']
-                if dE is not None:
-                    self._wavelengths = h_p * c / self.energies / erg_per_ev \
-                        / cm_per_ang
-                else:
-                    w1 = h_p * c / self.Emin / erg_per_ev / cm_per_ang
-                    w2 = h_p * c / self.Emax / erg_per_ev / cm_per_ang
-                    self._wavelengths = np.arange(w2, w1+dl, dl)
+                self._wavelengths = h_p * c / self.energies / erg_per_ev \
+                    / cm_per_ang
 
             if (self._wavelengths.max() < 2e3):
                 raise ValueError('Wavelengths all FUV. This is generally not a good idea!')
+
+            # Should be in ascending order
+            assert np.all(np.diff(self._wavelengths) > 0)
 
         return self._wavelengths
 
@@ -108,13 +104,13 @@ class SynthesisModelToy(SynthesisModelBase):
     def _StarQS(self):
         if not hasattr(self, '_StarQS_'):
 
-            assert self.pf['source_ssp'], "Must set source_ssp=True!"
+            #assert self.pf['source_ssp'], "Must set source_ssp=True!"
 
             from ..sources import StarQS
             kw = self.pf.copy()
             kw['source_sed'] = self.pf['source_toysps_method']
             self._StarQS_ = StarQS(cosm=self.cosm, **kw)
-            
+
         return self._StarQS_
 
     def _Spectrum(self, t, wave=1600.):
@@ -153,7 +149,10 @@ class SynthesisModelToy(SynthesisModelBase):
                 spec *= (t / trise)**1.5
         elif type(self.pf['source_toysps_method']) == str:
 
-            if t < (self._StarQS.lifetime / 1e6):
+            is_on = t < (self._StarQS.lifetime / 1e6) \
+                or (not self.pf['source_ssp'])
+
+            if is_on:
                 # This is normalized to Q in each sub-band.
                 E = h_p * c / (wave * cm_per_ang) / erg_per_ev
                 spec = self._StarQS.Spectrum(E)
@@ -191,8 +190,12 @@ class SynthesisModelToy(SynthesisModelBase):
                 # Now, [spec] = erg/s/eV
 
                 # Do last unit conversion to get erg/s/A/Msun
-                spec *= ev_per_hz / self.dwdn / mass
+                spec *= ev_per_hz / self.dwdn
 
+                if self.pf['source_ssp']:
+                    spec /= mass
+                else:
+                    spec /= (mass / self._StarQS.lifetime)
 
             else:
                 spec = 0.

ares/util/SetDefaultParameterValues.py
@@ -257,6 +257,7 @@ def PhysicsParameters():
     'feedback_LW_maxiter': 15,
     'feedback_LW_miniter': 0,
     'feedback_LW_softening': 'sqrt',
+    'feedback_LW_tol_zrange': (0, np.inf),
 
     'feedback_LW_Mmin_smooth': 0,
     'feedback_LW_Mmin_fit': 0,
@@ -593,9 +594,9 @@ def PopulationParameters():
     "pop_EmaxNorm": 8e3,
     "pop_Enorm": None,
 
-    "pop_lmin": 900,
-    "pop_lmax": 1e4,
-    "pop_dlam": 10.,
+    "pop_lmin": None,
+    "pop_lmax": None,
+    "pop_dlam": None,
     "pop_wavelengths": None,
     "pop_times": None,
 
@@ -847,7 +848,6 @@ def PopulationParameters():
     "pop_transition": 0,
 
     "pop_dE": None,
-    "pop_dlam": 1.,
 
     "pop_calib_wave": 1600,
     "pop_calib_lum": None,
@@ -868,9 +868,9 @@ def PopulationParameters():
     # Nebular emission stuff
     "pop_nebular_Tgas": 2e4,
 
-    "pop_lmin": 912.,
-    "pop_lmax": 1e4,
-    "pop_dlam": 10,
+    "pop_lmin": None,
+    "pop_lmax": None,
+    "pop_dlam": None,
     "pop_wavelengths": None,
     "pop_times": None,
 
@@ -952,10 +952,10 @@ def SourceParameters():
     "source_EminNorm": None,
     "source_EmaxNorm": None,
     "source_dE": None,
-    "source_dlam": 10.,
 
-    "source_lmin": 400.,
-    "source_lmax": 1e4,
+    "source_dlam": None,
+    "source_lmin": None,
+    "source_lmax": None,
     "source_wavelengths": None,
     "source_times": None,
 
@@ -1112,12 +1112,12 @@ def SynthesisParameters():
 
     # Only used by toy SPS
     "source_dE": None,
-    "source_dlam": 10.,
     "source_Emin": 1.,
     "source_Emax": 54.4,
 
-    "source_lmin": 400.,
-    "source_lmax": 1e4,
+    "source_dlam": None,
+    "source_lmin": None,
+    "source_lmax": None,
     "source_times": None,
     "source_wavelengths": None,
 