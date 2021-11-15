098120b0ed2aae2bebb81a1ca5310c702ba51206
Jordan Mirocha <mirochaj@gmail.com>
2021-11-15 13:29:40 -0500

    a few more get_lf call sequence updates

--- ares/inference/FitGalaxyPopulation.py
+++ ares/inference/FitGalaxyPopulation.py
@@ -163,8 +163,7 @@ class loglikelihood(LogLikelihood):
                     # observed magnitudes.
 
                     # Compute LF
-                    _x, p = pop.get_lf(z=zmod, bins=xdat,
-                        use_mags=True)
+                    _x, p = pop.get_lf(z=zmod, bins=xdat, use_mags=True)
 
                     if not np.isfinite(p):
                         print('WARNING: LF is inf or nan!', zmod, xdat, p)
