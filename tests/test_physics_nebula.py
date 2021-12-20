86aa74568409e88b2c239bcfb4bedbc9fe5453d3
Jordan Mirocha <mirochaj@gmail.com>
2021-12-20 16:24:14 -0500

    fixed up tests for nebular emission

--- tests/test_physics_nebula.py
+++ tests/test_physics_nebula.py
@@ -13,7 +13,7 @@ Description:
 import sys
 import ares
 import numpy as np
-from ares.physics.Constants import h_p, c, erg_per_ev, lam_LyA
+from ares.physics.Constants import h_p, c, erg_per_ev, lam_LyA, E_LL
 
 def test():
 
@@ -35,19 +35,16 @@ def test():
     pars_ares2.update(ares.util.ParameterBundle('testing:galaxies'))
     pars_ares2['pop_nebular'] = 2
     pars_ares2['pop_nebular_lookup'] = 'ferland1980'
-    pop_ares2 = ares.populations.GalaxyPopulation(**pars_ares)
+    pop_ares2 = ares.populations.GalaxyPopulation(**pars_ares2)
 
     # Setup source with BPASS-generated (CLOUDY) nebular emission
     pars_sps = ares.util.ParameterBundle('mirocha2017:base').pars_by_pop(0, 1)
     pars_sps.update(ares.util.ParameterBundle('testing:galaxies'))
     pars_sps['pop_nebular'] = 1
     pars_sps['pop_fesc'] = 0.
-    pars_sps['pop_nebula_Tgas'] = 2e4
+    pars_sps['pop_nebular_Tgas'] = 2e4
     pop_sps = ares.populations.GalaxyPopulation(**pars_sps)
 
-    code = 'bpass' if pars_ares['pop_sed'] == 'eldridge2009' else 's99'
-
-    colors = 'k', 'b', 'c', 'm', 'r'
     for k, t in enumerate([1, 5, 10, 20, 50]):
         i = np.argmin(np.abs(pop_ares.src.times - t))
 
@@ -62,6 +59,29 @@ def test():
             pop_ares2.src.wavelengths, pop_ares2.src.data[:,i])
         err = np.abs(y_ares - pop_sps.src.data[:,i]) / pop_sps.src.data[:,i]
         err2 = np.abs(y_ares2 - pop_sps.src.data[:,i]) / pop_sps.src.data[:,i]
+
+        Lion_H = pop_ares.src._nebula.get_ion_lum(pop_ares.src.data[:,i], 0)
+        Lion_He = pop_ares.src._nebula.get_ion_lum(pop_ares.src.data[:,i], 1)
+        Lion_He2 = pop_ares.src._nebula.get_ion_lum(pop_ares.src.data[:,i], 2)
+
+        assert Lion_H > Lion_He
+        assert Lion_He > Lion_He2
+
+        Nion_H = pop_ares.src._nebula.get_ion_num(pop_ares.src.data[:,i], 0)
+        Nion_He = pop_ares.src._nebula.get_ion_num(pop_ares.src.data[:,i], 1)
+        Nion_He2 = pop_ares.src._nebula.get_ion_num(pop_ares.src.data[:,i], 2)
+
+        assert Nion_H > Nion_He
+        assert Nion_He > Nion_He2
+
+        Eion_H = pop_ares.src._nebula.get_ion_Eavg(pop_ares.src.data[:,i], 0)
+        Eion_He = pop_ares.src._nebula.get_ion_Eavg(pop_ares.src.data[:,i], 1)
+        Eion_He2 = pop_ares.src._nebula.get_ion_Eavg(pop_ares.src.data[:,i], 2)
+
+        assert E_LL < Eion_H < 24.6
+        assert 24.6 < Eion_He < 4 * E_LL
+        assert Eion_He2 > E_LL * 4
+
 
     # Make sure emission at Ly-a is brighter for population with nebular
     # lines included.
@@ -78,6 +98,13 @@ def test():
     # nebular continuum+lines included.
     i1400 = np.argmin(np.abs(pop_ares.src.wavelengths - 1400))
     assert np.all(pop_ares.src.data[i1400,:] > pop_con.src.data[i1400,:])
+
+    # Check the Ferland (1980) vs default (Dopita & Sutherland) treatments
+    i1000 = np.argmin(np.abs(pop_ares.src.wavelengths - 1000.))
+    err = abs(pop_ares.src.data[i1000,:] - pop_ares2.src.data[i1000,:]) \
+        / pop_ares.src.data[i1000,:]
+    assert np.all(err <= 1e-2), \
+        "Ferland (1980) results should be closer to Dopita \& Sutherland!"
 
 
 if __name__ == '__main__':
