d958db2914a65bf0699cb908bfb1e039ef5b3ead
Jordan Mirocha <mirochaj@gmail.com>
2022-10-07 09:35:59 -0700

    a few edits to keep 'git status' clean

--- run_tests_local.sh
+++ run_tests_local.sh
@@ -3,4 +3,4 @@
 # Can never remember all the flags
 pytest --cov-config=.coveragerc --cov=ares --cov-report=html -v tests/*.py
 
-rm -f test_*.pkl test_*.txt test_*.hdf5
+rm -f test_*.pkl test_*.txt test_*.hdf5 hmf*.pkl hmf*.hdf5
