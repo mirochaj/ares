7f9dd3fcc4c11e9c812fd1d49c65c7b47b417a65
Jordan Mirocha <mirochaj@gmail.com>
2022-12-14 21:08:36 -0800

    added dev version of ebl parameter bundle

---
+++ input/litdata/ebldev.py
@@ -0,0 +1,104 @@
+import numpy as np
+from ares.physics.Constants import E_LyA, E_LL
+
+_base = \
+{
+ "halo_dt": 10,
+ "halo_tmin": 30.,
+ "halo_tmax": 13.7e3, # Myr
+ 'halo_mf': 'Tinker10',
+ "halo_mf_sub": 'Tinker08',
+
+ # NIRB
+ 'tau_approx': 'neutral',
+ 'tau_clumpy': 'madau1995',
+
+ 'cosmology_id': 'best',
+ 'cosmology_name': 'planck_TTTEEE_lowl_lowE',
+
+ 'final_redshift': 0,
+
+}
+
+
+centrals_sf = \
+{
+ 'pop_sfr_model': 'smhm-func',
+
+ 'pop_centrals': True,
+ 'pop_zdead': 0,
+ 'pop_include_1h': False,
+ 'pop_include_2h': True,
+ 'pop_include_shot': True,
+
+ # SED info
+ 'pop_sed': 'eldridge2009',
+ 'pop_binaries': False,
+ 'pop_rad_yield': 'from_sed',
+ 'pop_Emin': E_LyA,
+ 'pop_Emax': 24.6,
+ 'pop_fesc': 0.2,
+ 'pop_sed_degrade': 10,
+
+ # Something with dust and metallicity here
+
+ # fstar is SMHM for 'smhm-func' SFR model
+ 'pop_fstar': 'pq[0]',
+ 'pq_func[0]': 'dpl_evolNP',
+ 'pq_func_var[0]': 'Mh',
+ 'pq_func_var2[0]': '1+z',
+ 'pq_func_par0[0]': 3e-4,
+ 'pq_func_par1[0]': 1.5e12,
+ 'pq_func_par2[0]': 1.0,
+ 'pq_func_par3[0]': -0.4,
+ 'pq_func_par4[0]': 1e10,
+ 'pq_func_par5[0]': 1.,
+ 'pq_func_par6[0]': 0.0,   # norm
+ 'pq_func_par7[0]': 0.0,   # Mp
+ 'pq_func_par8[0]': 0.0,   # Only use if slopes evolve, e.g., in dplp_evolNPS
+ 'pq_func_par9[0]': 0.0,   # Only use if slopes evolve, e.g., in dplp_evolNPS
+ 'pq_val_ceil[0]': 1,
+
+# sSFR(z, Mstell)
+ #'pop_ssfr': 'pq[1]',
+ #'pq_func[1]': 'pl_evolN',
+ #'pq_func_var[1]': 'Ms',
+ #'pq_func_var2[1]': '1+z',
+ #'pq_func_par0[1]': 2e-10,
+ #'pq_func_par1[1]': 1e10,
+ #'pq_func_par2[1]': -0.0,
+ #'pq_func_par3[1]': 1.,
+ #'pq_func_par4[1]': 1.5,
+
+ 'pq_func[1]': 'dpl_evolN',
+ 'pq_func_var[1]': 'Ms',
+ 'pq_func_var2[1]': '1+z',
+ 'pq_func_par0[1]': 3e-10,
+ 'pq_func_par1[1]': 5e9,
+ 'pq_func_par2[1]': 0.0,
+ 'pq_func_par3[1]': -0.7,
+ 'pq_func_par4[1]': 1e9,
+ 'pq_func_par5[1]': 1.,
+ 'pq_func_par6[1]': 2.,
+
+ # Some occupation function stuff here.
+ 'pop_focc': 'pq[2]',
+ 'pq_func[2]': 'pl_evolN',
+ 'pq_func_var[2]': 'Mh',
+ 'pq_func_var2[2]': '1+z',
+ 'pq_val_ceil[2]': 1,
+ 'pq_func_par0[2]': 0.25,
+ 'pq_func_par1[2]': 1e11,
+ 'pq_func_par2[2]': -0.5,
+ 'pq_func_par3[2]': 0.5,
+ 'pq_func_par4[2]': 1,
+
+}
+
+centrals_sf.update(_base)
+
+centrals_q = centrals_sf.copy()
+centrals_q['pop_ssfr'] = None
+centrals_q['pop_ssp'] = True
+centrals_q['pop_age'] = 3e3
+centrals_q['pop_Z'] = 0.02
