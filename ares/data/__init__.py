118b621cef8df84333fecc4906d96958404077c4
Paul La Plante <plaplant@sas.upenn.edu>
2023-02-04 11:09:24 -0800

    Change where ares data directory is defined

--- ares/data/__init__.py
+++ ares/data/__init__.py
@@ -1,2 +1,6 @@
-_ARES = __path__[0]
-ARES = _ARES[0:_ARES.rfind('ares/')]
+HOME = os.getenv("HOME")
+ARES = f"{HOME}/.ares"
+
+# check that directory exists
+if not os.path.exists(ARES):
+    raise IOError("The directory ~/.ares does not exist. Please make it, or re-run package installation.")
