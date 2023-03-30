added dust_yield option for GalaxyCohort, fixed incorrect scaling of line luminosities (from Inoue) by Ha (should be Hb)

ares/populations/GalaxyCohort.py
@@ -480,6 +480,7 @@ class GalaxyCohort(GalaxyAggregate):
                 integrand = self.tab_sfr[i] * self.halos.tab_dndlnm[i] \
                     * self.tab_focc[i] * yield_per_sfr(**kw) * ok[i]
             else:
+                print("is this ever called")
                 # [erg/s]
                 lum_v_Mh = self.get_lum(z, band=(Emin, Emax),
                     band_units='eV')
@@ -941,7 +942,7 @@ class GalaxyCohort(GalaxyAggregate):
         #################
 
         # Use GalaxyAggregate's Emissivity function
-        if self.is_emissivity_scalable:
+        if self.is_emissivity_scalable and (not self.is_emissivity_bruteforce):
             # The advantage here is that the SFRD only has to be calculated
             # once, and the radiation field strength can just be determined
             # by scaling the SFRD.
@@ -953,8 +954,6 @@ class GalaxyCohort(GalaxyAggregate):
             # efficient because you're basically calculating the SFRD again
             # and again.
             rhoL = self._get_luminosity_density(Emin, Emax)(z)
-
-
 
         if E is not None:
             return rhoL * self.src.Spectrum(E) * on
@@ -1462,9 +1461,6 @@ class GalaxyCohort(GalaxyAggregate):
                 # updated from SAM.
                 sfr = self.get_sfr(z)
 
-                assert self.pf['pop_dust_yield'] in [None,0], \
-                    "pop_dust_yield must be zero for GalaxyCohort objects!"
-
                 # Just the product of SFR and L
                 if self.is_central_pop:
                     Lh = sfr * L_sfr
@@ -1498,10 +1494,14 @@ class GalaxyCohort(GalaxyAggregate):
                     T = self.dustext.get_T_lam(_wave, Av=Av)
 
                 Lh *= T
+            elif self.pf['pop_dust_yield'] is not None:
+                if type(wave) not in numeric_types:
+                    _wave = np.mean(wave)
+                else:
+                    _wave = wave
 
-
-            #if type(wave) not in numeric_types:
-            #    print('hi', self.id_num, wave, Lh.shape)
+                tau = self.get_dust_opacity(z=z, wave=_wave)
+                Lh *= np.exp(-tau)
 
             if not hasattr(self, '_cache_L'):
                 self._cache_L = {}
@@ -1570,6 +1570,53 @@ class GalaxyCohort(GalaxyAggregate):
             Ms = Mh * smhm
 
             return self._func_Av(z=z, Ms=Ms)
+        else:
+            raise NotImplemented('help')
+
+    def get_dust_opacity(self, z, wave):
+        """
+        Compute dust opacity for all galaxies at redshift `z` and wavelength
+        `wave`.
+
+        Parameters
+        ----------
+        z : int, float
+            Redshift
+        wave : int, float, np.ndarray
+            Wavelength [Angstroms]
+
+        Returns
+        -------
+        Opacity (dimensionless) for all halos in population. If input `wave` is
+        a scalar, returns an array of length `self.halos.tab_M`. If `wave` is
+        an array, the return will be a 2-D array with shape (self.halos.tab_M, len(waves))
+        """
+
+        if self.pf['pop_dust_yield'] is not None:
+            assert type(self.pf['pop_dust_yield']) in numeric_types
+
+            Rd = self.dust_scale(z=z, Mh=self.halos.tab_M)
+            smhm = self.get_smhm(z=z, Mh=self.halos.tab_M)
+            smhm[self.halos.tab_M < self.get_Mmin(z)] = 0
+            smhm[self.halos.tab_M > self.get_Mmax(z)] = 0
+            Ms = smhm * self.halos.tab_M
+            if self.pf['pop_metal_yield'] is not None:
+                MZ = self.pf['pop_metal_yield'] * Ms
+                Md = self.pf['pop_dust_yield'] * MZ
+            else:
+                Md = self.pf['pop_dust_yield'] * Ms
+
+            Sd = np.divide(Md, np.power(Rd, 2.)) / 4. / np.pi
+            Sd *= g_per_msun / cm_per_kpc**2
+
+            kappa = self.dust_kappa(wave=wave, z=z)
+            tau = kappa[None,:] * Sd[:,None]
+
+            if type(wave) in numeric_types:
+                return tau[:,0]
+            else:
+                return tau
+
         else:
             raise NotImplemented('help')
 

ares/populations/Population.py
@@ -405,7 +405,8 @@ class Population(object):
         nebular line emission?
         """
         return (self.pf['pop_nebular'] not in [0, 1]) or \
-               (self.pf['pop_dustext_template'] is not None)
+               (self.pf['pop_dustext_template'] is not None) or \
+               (self.pf['pop_dust_yield'] is not None)
 
     @property
     def is_emissivity_separable(self):
@@ -413,6 +414,10 @@ class Population(object):
         Are the frequency and redshift-dependent components independent?
         """
         return True
+
+    @cached_property
+    def is_emissivity_bruteforce(self):
+        return not self.pf['pop_emissivity_tricks']
 
     @property
     def is_emissivity_scalable(self):
@@ -929,6 +934,10 @@ class Population(object):
         """
         Tabulate emissivity over photon energy and redshift.
 
+        .. note :: This is not quite the emissivity -- it contains a factor of
+            the Hubble parameter and has units of photons, not erg, so as to
+            be more readily integrate-able in ares.solvers.UniformBackground.
+
         For a scalable emissivity, the tabulation is done for the emissivity
         in the (EminNorm, EmaxNorm) band because conversion to other bands
         can simply be applied after-the-fact. However, if the emissivity is
@@ -947,9 +956,7 @@ class Population(object):
         Returns
         -------
         A 2-D array, first axis corresponding to redshift, second axis for
-        photon energy.
-
-        Units of emissivity are: erg / s / Hz / cMpc
+        photon energy. Units are photons / s / Hz / (co-moving cm)^3.
 
         """
 
@@ -986,32 +993,37 @@ class Population(object):
 
         ##
         # Most general case: src.Spectrum does not contain all information.
-        if reprocessed:
+        if self.is_emissivity_bruteforce or reprocessed:
+            _waves = h_p * c * 1e8 / (E * erg_per_ev)
+
             for ll in range(Nz):
                 iz = np.argmin(np.abs(z[ll] - self.halos.tab_z))
 
                 ok = np.logical_and(self.halos.tab_M >= self.get_Mmin(z[ll]),
                     self.halos.tab_M < self.get_Mmax(z[ll]))
 
-                #print(ll, Nz)
                 for jj in range(Nf):
-                    _wave = h_p * c * 1e8 / (E[jj] * erg_per_ev)
 
                     # [erg/s/Hz]
-                    lum_v_Mh = self.get_lum(z[ll], wave=_wave)
+                    lum_v_Mh = self.get_lum(z[ll], wave=_waves[jj], raw=False)
 
-                    # Integrate over halo mass to get [erg/s/Hz/cMpc^3]
+                    # Setup integrand over population [erg/s/Hz/cMpc^3]
                     integrand = lum_v_Mh * self.halos.tab_dndlnm[iz,:] \
                         * self.tab_focc[iz,:] * ok
+
+                    # Integrate
                     _tot = np.trapz(integrand, x=np.log(self.halos.tab_M))
-                    _cumtot = cumtrapz(integrand, x=np.log(self.halos.tab_M),
-                        initial=0.0)
+                    #_cumtot = cumtrapz(integrand, x=np.log(self.halos.tab_M),
+                    #    initial=0.0)
 
-                    _tmp = _tot - \
-                        np.interp(np.log(self._tab_Mmin[iz]),
-                            np.log(self.halos.tab_M), _cumtot)
+                    #_tmp = _tot - \
+                    #    np.interp(np.log(self._tab_Mmin[iz]),
+                    #        np.log(self.halos.tab_M), _cumtot)
 
-                    epsilon[ll,jj] = _tmp / H[ll] / erg_per_ev / cm_per_mpc**3
+                    # Convert from luminosity in erg to photons
+                    epsilon[ll,jj] = _tot / H[ll] / (E[jj] * erg_per_ev)
+
+            epsilon /= cm_per_mpc**3
 
         elif scalable:
             Lbol = self.get_emissivity(z)

input/litdata/ebldev.py
@@ -210,14 +210,41 @@ disruption['pop_Mmin{5}'] = 1e10
 disruption['pop_fsurv{5}'] = 'link:fsurv:3'
 disruption['pop_fsurv_inv{5}'] = True
 
-dust = {}
-dust['pop_dustext_template'] = 'milkyway_rv4'
-dust['pop_Av'] = 'pq[4]'
-dust['pq_func[4]'] = 'pl_evolN'
-dust['pq_func_var[4]'] = 'Ms'
-dust['pq_func_var2[4]'] = '1+z'
-dust['pq_func_par0[4]'] = 1
-dust['pq_func_par1[4]'] = 1e10
-dust['pq_func_par2[4]'] = 0.1
-dust['pq_func_par3[4]'] = 1.  # Anchored to z=0
-dust['pq_func_par4[4]'] = 0.  # no evolution yet.
+dust_template = {}
+dust_template['pop_dustext_template'] = 'milkyway_rv4'
+dust_template['pop_Av'] = 'pq[4]'
+dust_template['pq_func[4]'] = 'pl_evolN'
+dust_template['pq_func_var[4]'] = 'Ms'
+dust_template['pq_func_var2[4]'] = '1+z'
+dust_template['pq_func_par0[4]'] = 1
+dust_template['pq_func_par1[4]'] = 1e10
+dust_template['pq_func_par2[4]'] = 0.1
+dust_template['pq_func_par3[4]'] = 1.  # Anchored to z=0
+dust_template['pq_func_par4[4]'] = 0.  # no evolution yet.
+
+dust = \
+{
+ 'pop_dust_yield': 0.4,
+
+ # Dust opacity vs. wavelength
+ "pop_dust_kappa": 'pq[20]',   # opacity in [cm^2 / g]
+ "pq_func[20]": 'pl',
+ 'pq_func_var[20]': 'wave',
+ 'pq_func_var_lim[20]': (0., np.inf),
+ 'pq_func_var_fill[20]': 0.0,
+ 'pq_func_par0[20]': 1e5,      # opacity at wavelength below
+ 'pq_func_par1[20]': 1e3,
+ 'pq_func_par2[20]': -1.,
+
+ # Screen parameters
+ 'pop_dust_fcov': 1,
+ "pop_dust_scale": 'pq[22]',       # Scale radius [in kpc]
+ "pq_func[22]": 'pl_evolN',
+ 'pq_func_var[22]': 'Mh',
+ 'pq_func_var2[22]': '1+z',
+ 'pq_func_par0[22]': 1.6,     # Note that Rhalo ~ Mh^1/3 / (1+z)
+ 'pq_func_par1[22]': 1e10,
+ 'pq_func_par2[22]': 0.45,
+ 'pq_func_par3[22]': 5.,
+ 'pq_func_par4[22]': 0.,
+}