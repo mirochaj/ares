few minor changes, and a fix in units in MGB that affected X-ray background flux, must have changed convention there at some pt

ares/physics/HaloModel.py
@@ -332,7 +332,7 @@ class HaloModel(HaloMassFunction):
             corr2 = 0.0
             fcoll2 = 1.
         else:
-            fcoll2 = self.fcoll_2d(z, np.log10(Mmin_2))#self.fcoll_Tmin[iz]
+            fcoll2 = self.tab_fcoll[iz,np.argmin(np.abs(mmin2-self.tab_M))]
             corr2 = 0.0
 
         ok = self.tab_fcoll[iz] > 0
@@ -459,6 +459,11 @@ class HaloModel(HaloMassFunction):
 
         iz, k, _prof1_, _prof2_ = self._prep_for_ps(z, k, None, None, ztol)
 
+        if lum1 is None:
+            lum1 = 1
+        if lum2 is None:
+            lum2 = 1
+            
         dndlnm = self.tab_dndlnm[iz]
         integrand = dndlnm * focc1 * lum1 * lum2
         shot = np.trapz(integrand, x=np.log(self.tab_M), axis=0)

ares/simulations/Global21cm.py
@@ -203,6 +203,23 @@ class Global21cm(AnalyzeGlobal21cm):
 
         return True
 
+    def get_21cm_dipole(self, vd_over_c=1.2e-3):
+        """
+
+        """
+        self.run()
+
+        nu, dTb = self.history['nu'], self.history['dTb']
+
+        from ..util.Math import central_difference
+
+        x, _y = central_difference(nu, dTb)
+        y = np.interp(nu, x, _y)
+        dTdn = -y * nu
+        dip = (dTb + dTdn) * vd_over_c
+
+        return dip
+
     def run(self):
         """
         Run a 21-cm simulation.
@@ -217,7 +234,6 @@ class Global21cm(AnalyzeGlobal21cm):
         if self.is_phenom:
             return
         if self.is_complete:
-            print("Already ran simulation!")
             return
 
         # Need to generate radiation backgrounds first.

ares/simulations/MetaGalacticBackground.py
@@ -357,6 +357,9 @@ class MetaGalacticBackground(AnalyzeMGB):
 
         #    f *= np.exp(-tau)
 
+        # Internal units are photons/s/cm^2/Hz, always put back into erg
+        f *= _E * erg_per_ev
+
         if units.lower() == 'cgs':
             pass
         elif units.lower() == 'si':
@@ -389,6 +392,7 @@ class MetaGalacticBackground(AnalyzeMGB):
         Parameters
         ----------
         band : tuple
+            Lower and upper bounds on interval of interest in `xunits`.
 
         """
 
@@ -397,29 +401,28 @@ class MetaGalacticBackground(AnalyzeMGB):
         assert units.lower() == 'cgs'
 
         fint = 0.0
-        xf, ff = self.get_spectrum(zf=zf, popids=popids, xunits=xunits,
-            units=units)
+
+
         for popid, pop in enumerate(self.pops):
-            if xf[popid] is None:
-                continue
 
             if popids is not None:
                 if popid not in popids:
                     continue
 
+            xf, ff = self.get_spectrum(zf=zf, popids=[popid], xunits=xunits,
+                units=units)
+            if xf[popid] is None:
+                continue
+
+            nu = (xf * erg_per_ev) / h_p
+
             lo, hi = band
 
-            flux_today = ff[popid] * xf[popid] \
-                * erg_per_ev / sqdeg_per_std / ev_per_hz
+            flux_today = ff
 
-            xok = np.logical_and(xf[popid] >= lo, xf[popid] <= hi)
+            xok = np.logical_and(xf >= lo, xf < hi)
 
-            xarr = xf[popid][xok]
-
-            # Find integrated flux
-            dlogx = np.diff(np.log10(xarr))
-
-            fint += np.trapz(flux_today[xok] * xarr, dx=dlogx) * np.log(10.)
+            fint += np.trapz(flux_today[xok] * nu[xok], x=np.log(nu[xok]))
 
         return fint
 

ares/util/Misc.py
@@ -213,7 +213,7 @@ def get_field_from_catalog(field, pos, Lbox, dims=512, mesh=None,
 
     data = np.array([_x, _y, _z]).T
     hist, edges = np.histogramdd(data, bins=[xe, ye, ze],
-        weights=field if weight_by_field else None, normed=False)
+        weights=field if weight_by_field else None, density=False)
 
     if by_volume:
         hist /= mesh**3

tests/test_populations_cohort_smhm.py
@@ -34,12 +34,13 @@ def test():
 
     focc = pop.get_focc(6, Mh)
     fsurv = pop.get_fsurv(6, Mh)
+    smhm = pop.get_smhm(z=6, Mh=Mh)
 
     assert not np.all(focc == 1)
     assert np.all(fsurv == 1)
 
-    sfr = pop.get_sfr(6, Mh)
-    ssfr = pop.get_ssfr(6, Mh)
+    sfr = pop.get_sfr(z=6, Mh=Mh)
+    ssfr = pop.get_ssfr(z=6, Ms=Mh*smhm)
 
     smd = sim.pops[1].get_smd(6)
     assert np.all(sim.pops[1].get_focc(6, Mh) == 1. - sim.pops[0].get_focc(6, Mh))