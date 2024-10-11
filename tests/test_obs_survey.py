ee30524dcd1f3f192470f9ce327967c04655397a
Jordan Mirocha <mirochaj@gmail.com>
2024-10-10 20:36:21 -0700

    scrapped get_dropout_filter function and associated test, not used anywhere

--- tests/test_obs_survey.py
+++ tests/test_obs_survey.py
@@ -16,16 +16,7 @@ from ares.obs import Survey, get_filters_from_waves
 def test():
     hst = Survey(cam='hst')
 
-    zarr = [4, 5, 6, 7, 8, 10]
-    drops = ['F435W', 'F606W', 'F775W', 'F850LP', 'F105W', 'F125W']
-    for (z, drop) in zip(zarr, drops):
-
-        fred, fblu = hst.get_dropout_filter(z,
-            drop_wave=912. if z < 6 else 1216., skip=['F110W'])
-
-        assert fred == drop, \
-            "Dropout filter at z={} should be {}, got {}".format(z, drop, fred)
-
+    for z in [4,5,6,7,8,10]:
         # Make sure we can recover filters over range in wavelength.
         hst_filt = hst.read_throughputs(filters='all')
         filt_p = get_filters_from_waves(z, hst_filt, picky=True)
