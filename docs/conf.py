2965e5015efdb7c1b5b2ae9e28fc3d99e5ac997a
Jordan Mirocha <mirochaj@gmail.com>
2024-10-11 13:28:15 -0700

    added lxml to conf.py's extensions

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode', 'numpydoc', 'nbsphinx', 'm2r2']
+    'sphinx.ext.viewcode', 'numpydoc', 'nbsphinx', 'm2r2', 'lxml']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ares'
-copyright = u'2015, Jordan Mirocha'
+copyright = u'2025, Jordan Mirocha'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
