18840c2ca114e3e3be3da48a977b051fd72302c1
Jordan Mirocha <mirochaj@gmail.com>
2025-02-26 11:08:14 -0800

    updating some docstrings, changing default precision of channel edges saved in map README to avoid rounding errors

--- ares/realizations/LightCone.py
+++ ares/realizations/LightCone.py
@@ -152,6 +152,11 @@ class LightCone(object): # pragma: no cover
         Determine the biggest field-of-view we can produce (without repeated
         structures) given the input box size (self.Lbox).
 
+        Parameters
+        ----------
+        zlim: tuple
+            Redshift range of interest.
+
         Returns
         -------
         Maximal field of view [degrees] in linear dimension.
@@ -169,7 +174,7 @@ class LightCone(object): # pragma: no cover
     def get_max_timestep(self):
         """
         Based on the size of our box, return the time interval corresponding to
-        along the z-axis for each chunk of our lightcone.
+        the z-axis for each chunk of our lightcone.
         """
 
         ze, zc, Re = self.get_domain_info()
@@ -260,12 +265,21 @@ class LightCone(object): # pragma: no cover
 
     def get_domain_info(self, zlim=None, Lbox=None):
         """
-        Figure out how domain will be divided up along line of sight.
+        Figure out how the domain will be divided up along the line of sight.
+
+        Parameters
+        ----------
+        zlim : tuple
+            Redshift range of interest.
+        Lbox : int, float
+            Co-eval box size in cMpc / h. If not provided, we'll use the
+            value in `self.Lbox`.
 
         Returns
         -------
         A tuple containing (chunk edges in redshift, chunk midpoints in redshift,
-            chunk edges in comoving Mpc).
+            chunk edges in comoving Mpc [NOT cMpc / h, despite input `Lbox`
+            being in cMpc/h!]).
 
         """
 
@@ -289,6 +303,10 @@ class LightCone(object): # pragma: no cover
     def get_redshift_chunks(self, zlim):
         """
         Return the edges of each co-eval cube as positioned along the LoS.
+
+        .. note :: Similar to output of `get_domain_info`, except redshift bins
+            are reported as 2-D array (series of bin edge pairs).
+
         """
 
         if self.zchunks is not None:
@@ -338,56 +356,6 @@ class LightCone(object): # pragma: no cover
             iz -= 1
 
         return iz
-
-    def thin_sample(self, max_sources=None):
-
-        if (max_sources is not None):
-            if (ct == 0) and (max_sources >= Mh.size):
-                # In this case, we can accommodate all the galaxies in
-                # the catalog, so don't do anything yet.
-                pass
-            else:
-                # Flag entries until we hit target.
-                # This is not efficient but oh well.
-                for h in range(Mh.size):
-                    ok[h] = 0
-
-                    if ok.sum() == max_sources:
-                        break
-
-                # This will be the final iteration.
-                if ct + ok.sum() == max_sources:
-                    self._hit_max_sources = True
-
-    #def get_base_dir(self, fov, pix):
-    #    """
-    #    Generate the name for the root directory where all mocks for a given
-    #    model will go.
-
-    #    Our model is:
-
-    #    -><base_dir>_fov_<FOV/deg>_pix_<pixel scale / arcsec>_L<box/cMpc/h>_N<dims>/
-    #    ->  README
-
-    #    Inside this directory, there will be many subdirectories: one for each
-    #    spectral channel of interest.
-
-    #    There will also be a series of .fits (or .hdf5) files, which represent
-    #    "final" maps, i.e., those that are summed over redshift and mass chunks,
-    #    and also summed over all source populations.
-
-    #    """
-
-
-    #    s = '{}/{}_fov_{:.1f}_pix_{:.1f}_L{:.0f}_N{:.0f}'.format(path,
-    #        self.prefix, fov, pix, self.Lbox, self.dims)
-
-    #    if suffix is None:
-    #        print("# WARNING: might be worth providing `suffix` as additional identifier.")
-    #    else:
-    #        s += f'_{self.model_name}'
-
-    #    return s
 
     def get_seed_kwargs(self, chunk, logmlim):
         # Deterministically adjust the random seeds for the given mass range
@@ -1270,6 +1238,21 @@ class LightCone(object): # pragma: no cover
         """
         Remember: we're using cgs units internally. This method determines the
         conversion factor to user's favorite `map_units` (within reason).
+
+        Parameters
+        ----------
+        map_units : str
+            Current options are 'si' (nW/m^2/sr^1), 'cgs' (erg/s/cm^2),
+            or 'MJy/sr'. Case insensivive.
+        pix : int, float
+            Pixel scale [arcseconds]. This is just in here because we are
+            generating fluxes *per pixel* first and so much convert to
+            per solid angle units.
+
+        Returns
+        -------
+        Normalization factor, i.e., if you multiply by this number it will
+        convert intensities *from* cgs *to* `map_units`.
         """
         if (map_units.lower() == 'si') or ('nw/m^2' in map_units.lower()):
             # aka (1e2)^2 / 0.01 = 1e6
@@ -1613,8 +1596,8 @@ class LightCone(object): # pragma: no cover
                             write_README = False
 
                 # channel name [optional]; central wavelength (microns); channel lower edge (microns) ; channel upper edge (microns) ; filename
-                s_ch  = f'{chname}; {np.mean(channel):.5f}; '
-                s_ch += f'{channel[0]:.5f}; {channel[1]:.5f}; '
+                s_ch  = f'{chname}; {np.mean(channel):.6f}; '
+                s_ch += f'{channel[0]:.5f}; {channel[1]:.6f}; '
                 s_ch += f'{popid}; {_fn} \n'
 
                 ##
@@ -1639,8 +1622,6 @@ class LightCone(object): # pragma: no cover
         # All done.
         pb.finish()
 
-        print(f"Doing post processing...")
-
         ##
         # Stitch together z slices?
         self.post_process_z_layers(fov, pix, channels,
@@ -1659,14 +1640,14 @@ class LightCone(object): # pragma: no cover
         """
         If we decided to save redshift layers, we may still need to sum
         together the individual mass layers.
-        """
 
-        print('in post_process_z_layers')
+        .. note :: Generalize this to automatically sum over source populations
+            as well?
+
+        """
 
         if not keep_layers:
             return
-
-        print('still in post_process_z_layers')
 
         # Full list of map layers to run.
         all_chunks = self.get_layers(channels, logmlim, dlogm=dlogm,
@@ -1704,8 +1685,6 @@ class LightCone(object): # pragma: no cover
                         fn = self.get_map_fn(fov, pix, channel, popid,
                             logmlim=mchunk, zlim=all_zchunks[iz])
 
-                        print(f"about to load {fn}.")
-
                         _buffer, _hdr = self._load_map(fn)
 
                         # Might need to adjust units before incrementing
@@ -1722,8 +1701,6 @@ class LightCone(object): # pragma: no cover
                     _fn = self.get_map_fn(fov, pix, channel, popid,
                         logmlim=logmlim, zlim=all_zchunks[iz])
 
-                    print(f"!! Saving z checkpoint {iz}: {_fn}")
-
                     self.save_map(_fn, cimg * f_norm / dnu,
                         channel, all_zchunks[iz], logmlim, fov,
                         pix=pix, fmt=fmt, hdr=hdr, map_units=map_units,
@@ -1734,8 +1711,20 @@ class LightCone(object): # pragma: no cover
         hdr={}, clobber=False, verbose=False, cat_units=''):
         """
         Save galaxy catalog.
+
+        Parameters
+        ----------
+        fn : str
+            Output filename.
+        cat : tuple
+            Contains four elements: (ra/deg, dec/deg, redshift, X), where
+            `X` is likely magnitude in some band, or SFR, etc.
+
         """
         ra, dec, red, X = cat
+
+        # Should just figure out `fmt` from filename in future
+        assert fn.endswith(fmt)
 
         if os.path.exists(fn) and (not clobber):
             if verbose:
