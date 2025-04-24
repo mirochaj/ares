a733d19d76c7177baefd4d2d919075b5ccee05eb
Jordan Mirocha <mirochaj@gmail.com>
2025-04-24 15:04:02 -0700

    slightly more careful with popid

--- ares/util/Misc.py
+++ ares/util/Misc.py
@@ -32,8 +32,8 @@ def get_pop_info(popid):
 
     # In this case, 'classic' behavior: just an integer, i.e.,
     # central galaxies.
-    if type(popid) == int:
-        return popid, popid, str(popid)
+    if (type(popid) == int) or (popid.isnumeric()):
+        return int(popid), int(popid), str(popid)
 
     if type(popid) == tuple:
         assert popid[1] < popid[0]
