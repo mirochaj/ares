0a1ad078246c55e8316e009bceb3419e860ccafb
Jordan Mirocha <jmirocha@Jordans-MacBook.local>
2025-09-09 14:48:51 -0700

    better error handling for dust_extinction and secondary_electron_data

--- ares/obs/DustExtinction.py
+++ ares/obs/DustExtinction.py
@@ -70,7 +70,8 @@ class DustExtinction(object):
     def is_template(self):
         is_templ = self.pf['pop_dust_template'] is not None
         if is_templ:
-            assert have_dustext
+            assert have_dustext, \
+                "Use of `pop_dust_template` requires `dustextinction` package!"
         return is_templ
 
     @cached_property
@@ -103,7 +104,8 @@ class DustExtinction(object):
     @property
     def _dustext_instance(self):
         if not hasattr(self, '_dustext_instance_'):
-            assert have_dustext, "Need dust_extinction package for this!"
+            assert have_dustext, \
+                "Use of `pop_dust_template` requires `dustextinction` package!"
             mth1, curve = self.method.split(':')
 
             self._dustext_instance_ = WD01(curve)
